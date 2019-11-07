0d2d139d957c5c1a47beecc551db125d19e42baa
Domagoj Salkić <dosalkic@vsite.hr>
2019-11-07 21:25:52 +0100

    Resolve type DateTime

--- KorištenjeObjekata/KorištenjeObjekata.cs
+++ KorištenjeObjekata/KorištenjeObjekata.cs
@@ -6,34 +6,28 @@ namespace Vsite.CSharp.DefiniranjeTipa
 {
     class KorištenjeObjekata
     {
-        // TODO:000 Ispisati trenutni datum i vrijeme pomoću statičkog svojstva DateTime.Now
         public static void IspišiTrenutniDatumVrijeme()
         {
-
+            Console.WriteLine(DateTime.Now);
         }
 
-        // TODO:001 Ispisati današnji datum pomoću statičkog svojstva DateTime.Now i metode DateTime.ToShortDateString
         public static void IspišiDanašnjiDatum()
         {
-
+            Console.WriteLine(DateTime.Now.ToShortDateString());
         }
 
-        // TODO:002 Ispisati trenutno vrijeme pomoću statičkog svojstva DateTime.Now i metode DateTime.ToShortTimeString
         public static void IspišiTrenutnoVrijeme()
         {
-
+            Console.WriteLine(DateTime.Now.ToShortTimeString());
         }
 
         public static int IzračunajBrojDanaIzmeđuDvaDatuma(int godina1, int mjesec1, int dan1, int godina2, int mjesec2, int dan2)
         {
-            // TODO:003 Stvoriti objekt "datum1" tipa DateTime tako da se njegovom konstruktoru proslijede godina1, mjesec1 i dan1.
+            DateTime datum1 = new DateTime(godina1, mjesec1, dan1);
+            DateTime datum2 = new DateTime(godina2, mjesec2, dan2);
+            TimeSpan razlika = datum2 - datum1;
 
-            // TODO:004 Stvoriti objekt "datum2" tipa DateTime tako da se njegovom konstruktoru proslijede godina2, mjesec2 i dan2.
-
-            // TODO:005 Izračunati razliku između datum1 i datum2 (jednostavnim oduzimanjem: datum2 - datum1) i tu razliku dodijeliti varijabli "period" tipa TimeSpan (koja služi za pohranu vremenskog perioda).
-
-            // TODO:006 Budući da metoda mora kao rezultat vratiti izračunati period izražen kao broj dana, iz prethodno izračunatog perioda treba dohvatiti i vratiti svojstvo Days (tj. svojstvo TimeSpan.Days).
-            return 0;
+            return razlika.Days;
         }
 
         static void Main(string[] args)
