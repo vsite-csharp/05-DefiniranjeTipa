d1d3b75e7314f574a73b3f847ca524542da0231d
Marko Vitković <mavitkov@vsite.hr>
2019-11-07 21:12:10 +0100

    done 03

--- Class-Struct/Class-Struct.cs
+++ Class-Struct/Class-Struct.cs
@@ -2,7 +2,7 @@
 
 namespace Vsite.CSharp.DefiniranjeTipa
 {
-    // TODO:020 Prekopirati kod klase iz prethodnog primjera i promijeniti pravo pristupa članu "broj" u public
+    
     class MojaKlasa
     {
 		public int broj = 5;
@@ -13,7 +13,7 @@ namespace Vsite.CSharp.DefiniranjeTipa
 
 	}
 
-	// TODO:024 Prekopirati kod strukture iz prethodnog primjera i promijeniti pravo pristupa članu "tekst" u public
+	
 	struct MojaStruktura
     {
 		public string tekst;
@@ -35,32 +35,40 @@ namespace Vsite.CSharp.DefiniranjeTipa
         {
             MojaKlasa mk1 = new MojaKlasa();
             MojaKlasa mk2 = mk1;
-            // TODO:021 Napisati naredbe koje pozivaju metodu IspišiČlan za obje instance.
+			mk1.IspisiClan();
+			mk2.IspisiClan();
+            
 
 
             Console.WriteLine();
-            // TODO:022 Otkomentirati donju naredbu.
-            //mk2.broj = 2;
-            // TODO:023 Ponovno pozvati metodu IspišiČlan za obje instance te obrazložiti rezultat.
+			
+			mk2.broj = 2;
+			
+			mk1.IspisiClan();
+			mk2.IspisiClan();
 
 
-        }
 
-        public static void IspišiStrukture()
+		}
+
+		public static void IspišiStrukture()
         {
-            // TODO:025 Promijeniti poziv konstruktora tako da ispis bude "MojaStruktura"
-            MojaStruktura ms1 = new MojaStruktura();
+            
+            MojaStruktura ms1 = new MojaStruktura("MojaStruktura");
             MojaStruktura ms2 = ms1;
-            // TODO:026 Pozvati metodu IspišiČlan za obje instance
+			ms1.IspisiClan();
+			ms2.IspisiClan();
+            
 
 
             Console.WriteLine();
-            // TODO:027 Otkomentirati donju naredbu.
-            //ms2.tekst = "MyStruct";
-            // TODO:028 Ponovno pozvati metodu IspišiČlan za obje instance te obrazložiti rezultat.
+			
+			ms2.tekst = "MyStruct";
+			
+			ms1.IspisiClan();
+			ms2.IspisiClan();
 
-
-        }
+		}
 
         static void Main(string[] args)
         {
