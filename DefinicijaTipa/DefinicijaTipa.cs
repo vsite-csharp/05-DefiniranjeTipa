44d583f192670810912335596a9286052ad71319
Bernard Šetinc <besetinc@vsite.hr>
2019-11-08 18:36:40 +0100

    Solve definiton type tests

--- DefinicijaTipa/DefinicijaTipa.cs
+++ DefinicijaTipa/DefinicijaTipa.cs
@@ -4,40 +4,48 @@ namespace Vsite.CSharp.DefiniranjeTipa
 {
     class MojaKlasa
     {
-        // TODO:010 Dodati privatno polje (field - podatkovni član) "broj" tipa int i dodijeliti mu vrijednost 5
+        private int broj = 5;
 
-
-        // TODO:011 Zadati metodu IspišiČlan tipa void i u njoj dodati naredbu koja će ispisati podatkovni član "broj".
-
+        public void IspišiČlan()
+        {
+            Console.WriteLine(broj);
+        }
     }
 
 
     struct MojaStruktura
     {
-        // TODO:013 Dodati privatno polje (podatkovni član) "tekst" tipa string i dodijeliti mu vrijednost "MojaStruktura"
-
-
-        // TODO:014 Zadati metodu IspišiČlan tipa void i u njoj dodati naredbu koja će ispisati podatkovni član "tekst".
-
+        private string tekst;
+        
+        public void IspišiČlan()
+        {
+            tekst = "MojaStruktura";
+            Console.WriteLine(tekst);
+        }
     }
 
 
     class DefinicijaTipa
     {
+        
+
         public static void IspišiČlanKlase()
         {
-            // TODO:012 Inicijalizirati objekt tipa MojaKlasa i pozvati njegovu metodu IspišiČlan
+            MojaKlasa klasa = new MojaKlasa();
+            klasa.IspišiČlan();
 
         }
 
         public static void IspišiČlanStrukture()
         {
-            // TODO:015 Inicijalizirati objekt tipa MojaStruktura i pozvati njegovu metodu IspišiČlan koja treba ispisati "MojaStruktura".
 
+            MojaStruktura struktura = new MojaStruktura();
+            struktura.IspišiČlan();
         }
 
         static void Main(string[] args)
         {
+            
             IspišiČlanKlase();
 
             IspišiČlanStrukture();
