9cc7d789cfd0a7dd813bed1a9d98a68c5ceac909
Bernard Šetinc <besetinc@vsite.hr>
2019-11-08 19:57:13 +0100

    Solve class constructor test

--- KonstruktorKlase/KonstruktorKlase.cs
+++ KonstruktorKlase/KonstruktorKlase.cs
@@ -7,12 +7,19 @@ namespace Vsite.CSharp.DefiniranjeTipa
         public static void StvoriObjektPodrazumijevanimKonstruktorom()
         {
             // TODO:060 Stvoriti objekt klase Osoba i ispisati vrijednosti članova Ime, Prezime, DatumRođenja
-
+            Osoba osoba = new Osoba();
+            Console.WriteLine(osoba.Ime);
+            Console.WriteLine(osoba.Prezime);
+            Console.WriteLine(osoba.DatumRođenja);
         }
 
         public static void StvoriObjektZadanimKonstruktorom(string ime, string prezime, DateTime datumRođenja)
         {
             // TODO:063 Stvoriti objekt klase Osoba pozivom tog konstruktora i ispisati vrijednosti članova Ime, Prezime, DatumRođenja
+            Osoba osoba1 = new Osoba("Nikola", "Tesla", new DateTime(1856, 7, 10));
+            Console.WriteLine(osoba1.Ime);
+            Console.WriteLine(osoba1.Prezime);
+            Console.WriteLine(osoba1.DatumRođenja);
 
         }
 
@@ -22,7 +29,7 @@ namespace Vsite.CSharp.DefiniranjeTipa
             StvoriObjektPodrazumijevanimKonstruktorom();
 
             StvoriObjektZadanimKonstruktorom("Hrvoje", "Horvat", new DateTime(1971, 7, 4));
-
+            
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey();
         }
--- KonstruktorKlase/Osoba.cs
+++ KonstruktorKlase/Osoba.cs
@@ -9,7 +9,20 @@ namespace Vsite.CSharp.DefiniranjeTipa
         public readonly DateTime DatumRođenja;
 
         // TODO:061 Dodati prazni konstruktor koji inicijalizira članove objekta na neku vrijednost, ponovno pokrenuti program i provjeriti ispis.
-
+       public Osoba()
+        {
+            Ime = "Pero";
+            Prezime = "Peric";
+            DatumRođenja = new DateTime(1990, 7, 5);
+        }
+        
+        
         // TODO:062 Promijeniti prazan konstruktor tako da prima 3 argumenta (ime, prezime, datumRođenja) kojima se inicijaliziraju članovi objekta.
+        public Osoba(string ime,string prezime,DateTime datumrođenja)
+        {
+            Ime = ime;
+            Prezime = prezime;
+            DatumRođenja = datumrođenja;
+        }
     }
 }
