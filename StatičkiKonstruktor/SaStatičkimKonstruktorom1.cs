05624e7f920120b7bb1502e7cae9b836e03393fa
Bernard Šetinc <besetinc@vsite.hr>
2019-11-09 09:23:54 +0100

    Solve static constructor tests

--- StatičkiKonstruktor/SaStatičkimKonstruktorom1.cs
+++ StatičkiKonstruktor/SaStatičkimKonstruktorom1.cs
@@ -4,12 +4,15 @@ namespace Vsite.CSharp.DefiniranjeTipa
 {
     class SaStatičkimKonstruktorom1
     {
-        // TODO:090 Dodati prazni konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom1.KonstruktorKlase"
-
-        // TODO:091 Dodati statički konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom1.StatičkiKonstruktor"
-
-        // TODO:092 Pokrenuti program i pogledati ispis
-
+        public SaStatičkimKonstruktorom1()
+        {
+            Console.WriteLine("SaStatičkimKonstruktorom1.KonstruktorKlase");
+        }
+        static SaStatičkimKonstruktorom1()
+        {
+            Console.WriteLine("SaStatičkimKonstruktorom1.StatičkiKonstruktor");
+        }
+        
         public static void Metoda1()
         {
             Console.WriteLine("SaStatičkimKonstruktorom1.Metoda1");
