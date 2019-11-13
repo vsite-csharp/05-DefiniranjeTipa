74d3032877e6add2aaadf436c322fe8adb1d6ba7
Krunoslav Bučar <krbuki@gmail.com>
2019-11-13 09:46:34 +0100

    Resolve Class-Struct

--- Class-Struct/Class-Struct.cs
+++ Class-Struct/Class-Struct.cs
@@ -2,16 +2,51 @@
 
 namespace Vsite.CSharp.DefiniranjeTipa
 {
-    // TODO:020 Prekopirati kod klase iz prethodnog primjera i promijeniti pravo pristupa članu "broj" u public
+    
     class MojaKlasa
     {
 
+        public int broj = 5;
+
+        public MojaKlasa(int broj)
+        {
+            this.broj = broj;
+
+        }
+        public int Broj
+        {
+            get { return broj; }
+            set { this.broj = value; }
+
+        }
+        public void IspišiČlan()
+        {
+            Console.WriteLine(broj);
+        }
     }
 
-    // TODO:024 Prekopirati kod strukture iz prethodnog primjera i promijeniti pravo pristupa članu "tekst" u public
+    
     struct MojaStruktura
     {
+        public string tekst;
+        public MojaStruktura(string tekst)
+        {
+            this.tekst = tekst;
+        }
+        public string Tekst
+        {
+            get
+            {
+                return tekst;
+            }
 
+
+        }
+
+        public void IspišiČlan()
+        {
+            Console.WriteLine(tekst);
+        }
 
     }
 
@@ -19,32 +54,41 @@ namespace Vsite.CSharp.DefiniranjeTipa
     {
         public static void IspišiKlase()
         {
-            MojaKlasa mk1 = new MojaKlasa();
+            int broj = 5;
+            MojaKlasa mk1 = new MojaKlasa(broj);
             MojaKlasa mk2 = mk1;
-            // TODO:021 Napisati naredbe koje pozivaju metodu IspišiČlan za obje instance.
-
-
+            
+            mk1.IspišiČlan();
+            mk2.IspišiČlan();
+            //ISPIS: 5 5
             Console.WriteLine();
-            // TODO:022 Otkomentirati donju naredbu.
-            //mk2.broj = 2;
-            // TODO:023 Ponovno pozvati metodu IspišiČlan za obje instance te obrazložiti rezultat.
-
-
+            
+            mk2.broj = 2;
+            
+            mk1.IspišiČlan();
+            mk2.IspišiČlan();
+            //ISPIS: 2 2
         }
 
         public static void IspišiStrukture()
         {
-            // TODO:025 Promijeniti poziv konstruktora tako da ispis bude "MojaStruktura"
-            MojaStruktura ms1 = new MojaStruktura();
+            
+            MojaStruktura ms1 = new MojaStruktura("MojaStruktura");
             MojaStruktura ms2 = ms1;
-            // TODO:026 Pozvati metodu IspišiČlan za obje instance
-
+            
+            ms1.IspišiČlan();
+            ms2.IspišiČlan();
+            // ISPIS:         MojaStruktura
+            //               MojaStruktura
 
             Console.WriteLine();
-            // TODO:027 Otkomentirati donju naredbu.
-            //ms2.tekst = "MyStruct";
-            // TODO:028 Ponovno pozvati metodu IspišiČlan za obje instance te obrazložiti rezultat.
-
+            
+            ms2.tekst = "MyStruct";
+            
+            ms1.IspišiČlan();
+            ms2.IspišiČlan();
+            //ISPIS:  MojaStruktura
+           //          MyStruct
 
         }
 
