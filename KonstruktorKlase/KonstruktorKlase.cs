8dc10adf6a91915833046443c38ae493071ac295
Krunoslav Bučar <krbuki@gmail.com>
2019-11-14 21:20:05 +0100

    Resolve constuctor delegation

--- KonstruktorKlase/KonstruktorKlase.cs
+++ KonstruktorKlase/KonstruktorKlase.cs
@@ -2,18 +2,26 @@
 
 namespace Vsite.CSharp.DefiniranjeTipa
 {
-    class KonstruktorKlase
+    class KonstruktorKlase //DZ
     {
         public static void StvoriObjektPodrazumijevanimKonstruktorom()
         {
             // TODO:060 Stvoriti objekt klase Osoba i ispisati vrijednosti članova Ime, Prezime, DatumRođenja
-
+            Osoba o1 = new Osoba(); //f12
+            
+            Console.WriteLine(o1.Ime);
+            Console.WriteLine(o1.Prezime);
+            Console.WriteLine(o1.DatumRođenja);
+            //1.1.0001. 0:00:00
         }
 
         public static void StvoriObjektZadanimKonstruktorom(string ime, string prezime, DateTime datumRođenja)
         {
             // TODO:063 Stvoriti objekt klase Osoba pozivom tog konstruktora i ispisati vrijednosti članova Ime, Prezime, DatumRođenja
-
+            Osoba o2 = new Osoba("Pero", "Perić", DateTime.MinValue);
+            Console.WriteLine(o2.Ime);
+            Console.WriteLine(o2.Prezime);
+            Console.WriteLine(o2.DatumRođenja);
         }
 
 
--- KonstruktorKlase/Osoba.cs
+++ KonstruktorKlase/Osoba.cs
@@ -2,14 +2,30 @@
 
 namespace Vsite.CSharp.DefiniranjeTipa
 {
-    class Osoba
+    class Osoba   //DZ
     {
         public readonly string Ime;
         public readonly string Prezime;
         public readonly DateTime DatumRođenja;
 
         // TODO:061 Dodati prazni konstruktor koji inicijalizira članove objekta na neku vrijednost, ponovno pokrenuti program i provjeriti ispis.
-
+        public Osoba()//prazni konstruktor...članovi gore će imati vrijednost
+        {
+            Ime = "Milan";
+            Prezime = "Bandić";
+            DatumRođenja = new DateTime(1756,12,3);
+            /*Nakon gornjih izmjena ispis: Milan
+Bandic
+3.12.1756. 0:00:00*/
+        }
         // TODO:062 Promijeniti prazan konstruktor tako da prima 3 argumenta (ime, prezime, datumRođenja) kojima se inicijaliziraju članovi objekta.
+        public Osoba(string ime,string prezime,DateTime datumRođenja)
+        {
+            Ime = ime;
+            Prezime = prezime;
+            DatumRođenja =datumRođenja;
+        }
+       
+        
     }
 }
