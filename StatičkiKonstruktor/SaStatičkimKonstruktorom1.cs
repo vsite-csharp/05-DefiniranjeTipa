60562db042546672e7fafc3ee7a0a825d7f029de
Auro Mazzi <auro.mazzi@gmail.com>
2019-11-14 21:30:30 +0100

    Resolve static constructor

--- StatičkiKonstruktor/SaStatičkimKonstruktorom1.cs
+++ StatičkiKonstruktor/SaStatičkimKonstruktorom1.cs
@@ -5,9 +5,15 @@ namespace Vsite.CSharp.DefiniranjeTipa
     class SaStatičkimKonstruktorom1
     {
         // TODO:090 Dodati prazni konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom1.KonstruktorKlase"
-
         // TODO:091 Dodati statički konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom1.StatičkiKonstruktor"
-
+        public SaStatičkimKonstruktorom1()
+        {
+            Console.WriteLine("SaStatičkimKonstruktorom1.KonstruktorKlase");
+        }
+        static SaStatičkimKonstruktorom1()
+        {
+            Console.WriteLine("SaStatičkimKonstruktorom1.StatičkiKonstruktor");
+        }
         // TODO:092 Pokrenuti program i pogledati ispis
 
         public static void Metoda1()
--- StatičkiKonstruktor/SaStatičkimKonstruktorom2.cs
+++ StatičkiKonstruktor/SaStatičkimKonstruktorom2.cs
@@ -5,9 +5,15 @@ namespace Vsite.CSharp.DefiniranjeTipa
     class SaStatičkimKonstruktorom2 : SaStatičkimKonstruktorom1
     {
         // TODO:093 Dodati prazni konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom2.KonstruktorKlase"
-
         // TODO:094 Dodati statički konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom2.StatičkiKonstruktor"
-
+        public SaStatičkimKonstruktorom2()
+        {
+            Console.WriteLine("SaStatičkimKonstruktorom2.KonstruktorKlase");
+        }
+        static SaStatičkimKonstruktorom2()
+        {
+            Console.WriteLine("SaStatičkimKonstruktorom2.StatičkiKonstruktor");
+        }
         // TODO:095 Ponovno pokrenuti program i pogledati ispis
 
         public void Metoda2()
