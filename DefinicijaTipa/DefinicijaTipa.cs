cf6ec679627a247bff8454a9a83c0e06216eb5aa
Marko Bohač <mabohac@vsite.hr>
2019-11-14 19:47:42 +0100

    Resolved Class and Struct fields

--- DefinicijaTipa/DefinicijaTipa.cs
+++ DefinicijaTipa/DefinicijaTipa.cs
@@ -4,21 +4,25 @@ namespace Vsite.CSharp.DefiniranjeTipa
 {
     class MojaKlasa
     {
-        // TODO:010 Dodati privatno polje (field - podatkovni član) "broj" tipa int i dodijeliti mu vrijednost 5
+        private int broj = 5; // private je inace po defaultu
 
-
-        // TODO:011 Zadati metodu IspišiČlan tipa void i u njoj dodati naredbu koja će ispisati podatkovni član "broj".
-
+        public void IspišiČlan() {
+            Console.WriteLine(broj);
+        }
     }
 
 
     struct MojaStruktura
     {
-        // TODO:013 Dodati privatno polje (podatkovni član) "tekst" tipa string i dodijeliti mu vrijednost "MojaStruktura"
+        private String text;
+        public MojaStruktura(String text) {
+            this.text = text;
+        }
 
-
-        // TODO:014 Zadati metodu IspišiČlan tipa void i u njoj dodati naredbu koja će ispisati podatkovni član "tekst".
-
+        public void IspišiČlan()
+        {
+            Console.WriteLine(text);
+        }
     }
 
 
@@ -26,8 +30,8 @@ namespace Vsite.CSharp.DefiniranjeTipa
     {
         public static void IspišiČlanKlase()
         {
-            // TODO:012 Inicijalizirati objekt tipa MojaKlasa i pozvati njegovu metodu IspišiČlan
-
+            MojaKlasa mk = new MojaKlasa();
+            mk.IspišiČlan();
         }
 
         public static void IspišiČlanStrukture()
