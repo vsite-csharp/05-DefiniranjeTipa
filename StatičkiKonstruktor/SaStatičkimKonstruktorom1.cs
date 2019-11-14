f8dee88487b25215d95f96ccd7b73e9a58453268
Paško Berišić <paberisi@vsite.hr>
2019-11-14 21:31:02 +0100

    Resolve static construcotr

--- StatičkiKonstruktor/SaStatičkimKonstruktorom1.cs
+++ StatičkiKonstruktor/SaStatičkimKonstruktorom1.cs
@@ -4,15 +4,15 @@ namespace Vsite.CSharp.DefiniranjeTipa
 {
     class SaStatičkimKonstruktorom1
     {
-        // TODO:091 Dodati prazni konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom1.KonstruktorKlase"
-
-        // TODO:092 Dodati statički konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom1.StatičkiKonstruktor"
-
-        // TODO:093 Pokrenuti program i pogledati ispis
-
+        
+        static SaStatičkimKonstruktorom1()
+        {
+            Console.WriteLine("SaStatičkimKonstruktorom1.KonstruktorKlase");
+        }
         public static void Metoda1()
         {
             Console.WriteLine("SaStatičkimKonstruktorom1.Metoda1");
         }
+
     }
 }
