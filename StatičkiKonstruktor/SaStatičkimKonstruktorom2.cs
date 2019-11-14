451a4db5ac8d1419aa75a109c88959b364609afe
Igor Obradovic <iobradovic@ZAWMIOB>
2019-11-14 21:30:23 +0100

    Resolve static constructor.

--- StatičkiKonstruktor/SaStatičkimKonstruktorom2.cs
+++ StatičkiKonstruktor/SaStatičkimKonstruktorom2.cs
@@ -9,6 +9,15 @@ namespace Vsite.CSharp.DefiniranjeTipa
         // TODO:095 Dodati statički konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom2.StatičkiKonstruktor"
 
         // TODO:096 Ponovno pokrenuti program i pogledati ispis
+        public SaStatičkimKonstruktorom2()
+        {
+            Console.WriteLine("SaStatičkimKonstruktorom2.KonstruktorKlase");
+        }
+
+        static SaStatičkimKonstruktorom2()
+        {
+            Console.WriteLine("SaStatičkimKonstruktorom2.StatičkiKonstruktor");
+        }
 
         public void Metoda2()
         {
