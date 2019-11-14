60075bee61427ca40250f4010333cb35ed530791
Roland Rostohar <rorostoh@vsite.hr>
2019-11-14 20:50:05 +0100

    Resolve static members

--- StatičkiČlanovi/BrojačInstanci.cs
+++ StatičkiČlanovi/BrojačInstanci.cs
@@ -4,27 +4,24 @@ namespace Vsite.CSharp.StatičkiČlanovi
 {
     class BrojačInstanci
     {
-        // TODO:050 Dodati privatni statički član "brojač" tipa int i inicijalizirati ga na 0
+		private static int brojac = 0;
 
-
-        // TODO:051 U metodu dodati naredbu koja će ispisati trenutnu vrijednost člana brojač
         public static void IspišiBrojStvorenihObjekata()
         {
-
+			Console.WriteLine(brojac);
         }
 
-        // TODO:052 U klasu dodati privatni član "instanca" tipa int
+		private int instanca;
 
-        // TODO:053 U konstruktor dodati naredbu koja će uvećati "brojač" za 1 i uvećanu vrijednost dodijeliti članu "instanca"
         public BrojačInstanci()
         {
-
+			++brojac;
+			instanca = brojac;
         }
 
-        // TODO:054 U metodu dodati naredbu koja će ispisati vrijednost člana "instanca"
         public void IspišiRedniBrojObjekta()
         {
-
+			Console.WriteLine(instanca);
         }
     }
 }
