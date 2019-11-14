e10b2df8827f4152750696dca67e65aaa265ba3b
Marko Vitković <mavitkov@vsite.hr>
2019-11-14 19:57:16 +0100

    done 02

--- Class-Struct/Class-Struct.cs
+++ Class-Struct/Class-Struct.cs
@@ -2,47 +2,64 @@
 
 namespace Vsite.CSharp.DefiniranjeTipa
 {
-    // TODO:020 Prekopirati kod strukture iz prethodnog primjera i promijeniti pravo pristupa članu "tekst" u public
+    
     struct MojaStruktura
     {
-
+		public string tekst;
+		public MojaStruktura(string s)
+		{
+			tekst = s;
+		}
+		public void IspisiClan()
+		{
+			Console.WriteLine(tekst);
+		}
 
     }
 
-    // TODO:025 Prekopirati kod klase iz prethodnog primjera i promijeniti pravo pristupa članu "broj" u public
+   
     class MojaKlasa
     {
-
+		public int br = 5;
+		public void IspisiClan()
+		{
+			Console.WriteLine(br);
+		}
     }
 
     class ClassStruct
     {
         public static void IspišiStrukture()
         {
-            // TODO:021 Promijeniti poziv konstruktora tako da ispis bude "MojaStruktura"
-            MojaStruktura ms1 = new MojaStruktura();
+            
+            MojaStruktura ms1 = new MojaStruktura("Moja struktura");
             MojaStruktura ms2 = ms1;
-            // TODO:022 Pozvati metodu IspišiČlan za obje instance
+			ms1.IspisiClan();
+			ms2.IspisiClan();
+            
 
             Console.WriteLine();
-            // TODO:023 Otkomentirati donju naredbu.
-            //ms2.tekst = "MyStruct";
-            // TODO:024 Ponovno pozvati metodu IspišiČlan za obje instance te obrazložiti rezultat.
+            
+            ms2.tekst = "MyStruct";
+			ms1.IspisiClan();
+			ms2.IspisiClan();
 
-        }
+		}
 
         public static void IspišiKlase()
         {
             MojaKlasa mk1 = new MojaKlasa();
             MojaKlasa mk2 = mk1;
-            // TODO:026 Napisati naredbe koje pozivaju metodu IspišiČlan za obje instance.
+			mk1.IspisiClan();
+			mk2.IspisiClan();
 
-            Console.WriteLine();
-            // TODO:027 Otkomentirati donju naredbu.
-            //mk2.broj = 2;
-            // TODO:028 Ponovno pozvati metodu IspišiČlan za obje instance te obrazložiti rezultat.
-
-        }
+			Console.WriteLine();
+			
+			mk2.br = 2;
+			
+			mk1.IspisiClan();
+			mk2.IspisiClan();
+		}
 
         static void Main(string[] args)
         {
