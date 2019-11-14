ec06f26abb4f9c1161b83bd17344cb521e727cc8
Senad Elezović <sen.elezovic@gmail.com>
2019-11-14 21:20:31 +0100

    Delegiranje konstruktora

--- DelegiranjeKonstruktora/DelegiranjeKonstruktora.cs
+++ DelegiranjeKonstruktora/DelegiranjeKonstruktora.cs
@@ -7,18 +7,17 @@ namespace Vsite.CSharp.DefiniranjeTipa
 
         public static void StvoriInstancuIzvedeneKlaseKonstruktoromSDvaArgumenta(int a, int b)
         {
-            // TODO:082 Stvoriti objekt pozivom tog konstruktora:
-            //Izvedena i = new Izvedena(a, b);
-
-            // TODO:083 Dodati dvije naredbe koje će ispisati vrijednosti članova A i B stvorenog objekta i, pokrenuti kod i provjeriti ispis.
+            
+            Izvedena i = new Izvedena(a, b);
+            Console.WriteLine(i.A);
+            Console.WriteLine(i.B);
         }
 
         public static void StvoriInstancuIzvedeneKlaseKonstruktoromSJednimArgumentom(int a)
         {
-            // TODO:086 Stvoriti objekt pozivom tog konstruktora:
-            // Izvedena i = new Izvedena(a);
-
-            // TODO:087 Dodati dvije naredbe koje će ispisati vrijednosti članova A i B stvorenog objekta i, pokrenuti kod i provjeriti ispis.
+             Izvedena i = new Izvedena(a);
+            Console.WriteLine(i.A);
+            Console.WriteLine(i.B);
         }
 
         static void Main(string[] args)
