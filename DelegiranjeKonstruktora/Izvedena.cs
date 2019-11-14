f6125ce20b417f8517f2bc2ae9ac766732439af5
Luka Rađenović <luradjen@vsite.hr>
2019-11-14 21:20:03 +0100

    Resolved constructor delegation

--- DelegiranjeKonstruktora/Izvedena.cs
+++ DelegiranjeKonstruktora/Izvedena.cs
@@ -1,14 +1,17 @@
-﻿namespace Vsite.CSharp.DefiniranjeTipa
+﻿using System;
+namespace Vsite.CSharp.DefiniranjeTipa
 {
-    class Izvedena // : Bazna
+    class Izvedena : Bazna
     {
         public readonly int B;
 
-        // TODO:080 Dodati konstruktor s dva argumenta ("a" i "b") tipa int, kojima će se inicijalizirati članovi A i B. Konstruktor treba pozivati konstruktor bazne klase s jednim argumentom i proslijediti mu argument a.
-        // TODO:081 U tijelo konstruktora dodati naredbu za ispis: Console.WriteLine(string.Format("Izvedena.Izvedena({0}, {1})", a, b));
+        public Izvedena(int a, int b) : base(a) {
+            B = b;
+            Console.WriteLine(string.Format("Izvedena.Izvedena({0}, {1})", a, b));
+        }
 
-        // TODO:084 Dodati konstruktor s jednim argumentom "a" tipa int. Taj konstruktor treba pozvati ("delegirati") prethodno definirani konstruktor klase Izvedena s dva argumenta s time da je prvi argument "a", a drugi argument je 0. 
-        // TODO:085 U tijelo konstruktora dodati naredbu za ispis: Console.WriteLine(string.Format("Izvedena.Izvedena({0})", a));
-
+        public Izvedena(int a) : this(a, 0) {
+            Console.WriteLine(string.Format("Izvedena.Izvedena({0})", a));
+        }
     }
 }
