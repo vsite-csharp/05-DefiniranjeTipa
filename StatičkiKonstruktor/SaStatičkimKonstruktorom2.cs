92a5f99ea93f1e05a4965a48d2787dae0af2d4fc
esvegot <esvegot@HR-00004162>
2019-11-14 21:30:23 +0100

    Resolve static CTORs

--- StatičkiKonstruktor/SaStatičkimKonstruktorom2.cs
+++ StatičkiKonstruktor/SaStatičkimKonstruktorom2.cs
@@ -4,6 +4,17 @@ namespace Vsite.CSharp.DefiniranjeTipa
 {
     class SaStatičkimKonstruktorom2 : SaStatičkimKonstruktorom1
     {
+        public SaStatičkimKonstruktorom2()
+        {
+            Console.WriteLine("SaStatičkimKonsturktorom2.KonstruktorKlase");
+        }
+        //  Dodati statički konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom1.StatičkiKonstruktor"
+        static SaStatičkimKonstruktorom2()
+        {
+            Console.WriteLine("SaStatičkimKonsturktorom2.StatičkiKonstruktor");
+        }
+        //  Pokrenuti program i pogledati ispis
+
         // TODO:094 Dodati prazni konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom2.KonstruktorKlase"
 
         // TODO:095 Dodati statički konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom2.StatičkiKonstruktor"
