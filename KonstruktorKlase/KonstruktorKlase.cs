432dd1331f834bf19dc18787902307bd701ba2e6
Paola Kolar <kolar.paola@gmail.com>
2019-11-14 21:06:42 +0100

    Resolve constructor

--- KonstruktorKlase/KonstruktorKlase.cs
+++ KonstruktorKlase/KonstruktorKlase.cs
@@ -6,14 +6,18 @@ namespace Vsite.CSharp.DefiniranjeTipa
     {
         public static void StvoriObjektPodrazumijevanimKonstruktorom()
         {
-            // TODO:060 Stvoriti objekt klase Osoba i ispisati vrijednosti članova Ime, Prezime, DatumRođenja
-
+            Osoba osoba = new Osoba("","",DateTime.MinValue);
+            Console.WriteLine(osoba.Ime);
+            Console.WriteLine(osoba.Prezime);
+            Console.WriteLine(osoba.DatumRođenja);
         }
 
         public static void StvoriObjektZadanimKonstruktorom(string ime, string prezime, DateTime datumRođenja)
         {
-            // TODO:063 Stvoriti objekt klase Osoba pozivom tog konstruktora i ispisati vrijednosti članova Ime, Prezime, DatumRođenja
-
+            Osoba osoba = new Osoba(ime, prezime, datumRođenja);
+            Console.WriteLine(osoba.Ime);
+            Console.WriteLine(osoba.Prezime);
+            Console.WriteLine(osoba.DatumRođenja);
         }
 
 
--- KonstruktorKlase/Osoba.cs
+++ KonstruktorKlase/Osoba.cs
@@ -4,12 +4,16 @@ namespace Vsite.CSharp.DefiniranjeTipa
 {
     class Osoba
     {
-        public readonly string Ime;
+        public readonly string Ime; //readonly može se inicijalizirati samo u konstruktoru i nigdje poslije
         public readonly string Prezime;
         public readonly DateTime DatumRođenja;
 
         // TODO:061 Dodati prazni konstruktor koji inicijalizira članove objekta na neku vrijednost, ponovno pokrenuti program i provjeriti ispis.
-
+        public Osoba(string ime, string prezime, DateTime datumRođenja){
+            Ime = ime;
+            Prezime = prezime;
+            DatumRođenja = datumRođenja;
+        }
         // TODO:062 Promijeniti prazan konstruktor tako da prima 3 argumenta (ime, prezime, datumRođenja) kojima se inicijaliziraju članovi objekta.
     }
 }
