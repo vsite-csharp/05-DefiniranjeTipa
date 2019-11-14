4a2dcfc208e1a21f44f11d940914405031b33e62
Roland Rostohar <rorostoh@vsite.hr>
2019-11-14 19:48:21 +0100

    Resolve type definition

--- DefinicijaTipa/DefinicijaTipa.cs
+++ DefinicijaTipa/DefinicijaTipa.cs
@@ -4,36 +4,43 @@ namespace Vsite.CSharp.DefiniranjeTipa
 {
     class MojaKlasa
     {
-        // TODO:010 Dodati privatno polje (field - podatkovni član) "broj" tipa int i dodijeliti mu vrijednost 5
+		
+		private int broj = 5;
 
-
-        // TODO:011 Zadati metodu IspišiČlan tipa void i u njoj dodati naredbu koja će ispisati podatkovni član "broj".
-
+		public void IspisiClan()
+		{
+			Console.WriteLine(broj);
+		}
     }
 
 
     struct MojaStruktura
     {
-        // TODO:013 Dodati privatno polje (podatkovni član) "tekst" tipa string i dodijeliti mu vrijednost "MojaStruktura"
+
+		public MojaStruktura(string tekst) { this.tekst = "Moja struktura"; }
 
 
-        // TODO:014 Zadati metodu IspišiČlan tipa void i u njoj dodati naredbu koja će ispisati podatkovni član "tekst".
+		private string tekst;
 
-    }
+		public void IspisiClan()
+		{
+			Console.WriteLine(tekst);
+		}
+	}
 
 
     class DefinicijaTipa
     {
         public static void IspišiČlanKlase()
         {
-            // TODO:012 Inicijalizirati objekt tipa MojaKlasa i pozvati njegovu metodu IspišiČlan
-
+			MojaKlasa mk = new MojaKlasa();
+			mk.IspisiClan();
         }
 
         public static void IspišiČlanStrukture()
         {
-            // TODO:015 Inicijalizirati objekt tipa MojaStruktura i pozvati njegovu metodu IspišiČlan koja treba ispisati "MojaStruktura".
-
+			var ms = new MojaStruktura("MojaStruktura");
+			ms.IspisiClan();
         }
 
         static void Main(string[] args)
