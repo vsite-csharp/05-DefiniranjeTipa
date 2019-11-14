73506144d8b949b886a0f216ac806b37405f5a57
Fran Safranec <Fran@DESKTOP-QN7J9VS>
2019-11-14 19:48:24 +0100

    resolve type definition

--- DefinicijaTipa/DefinicijaTipa.cs
+++ DefinicijaTipa/DefinicijaTipa.cs
@@ -4,20 +4,27 @@ namespace Vsite.CSharp.DefiniranjeTipa
 {
     class MojaKlasa
     {
-        // TODO:010 Dodati privatno polje (field - podatkovni član) "broj" tipa int i dodijeliti mu vrijednost 5
+        private int broj = 5;
 
-
-        // TODO:011 Zadati metodu IspišiČlan tipa void i u njoj dodati naredbu koja će ispisati podatkovni član "broj".
-
+        public void ispisiClan()
+        {
+            Console.WriteLine(broj);
+        }
     }
 
 
     struct MojaStruktura
     {
-        // TODO:013 Dodati privatno polje (podatkovni član) "tekst" tipa string i dodijeliti mu vrijednost "MojaStruktura"
+        public MojaStruktura(string text)
+        {
+            this.text = text;
+        }
+        private string text;
 
-
-        // TODO:014 Zadati metodu IspišiČlan tipa void i u njoj dodati naredbu koja će ispisati podatkovni član "tekst".
+        public void ispisiClan()
+        {
+            Console.WriteLine(text);
+        }
 
     }
 
@@ -26,14 +33,15 @@ namespace Vsite.CSharp.DefiniranjeTipa
     {
         public static void IspišiČlanKlase()
         {
-            // TODO:012 Inicijalizirati objekt tipa MojaKlasa i pozvati njegovu metodu IspišiČlan
+            MojaKlasa mk = new MojaKlasa();
+            mk.ispisiClan();
 
         }
 
         public static void IspišiČlanStrukture()
         {
-            // TODO:015 Inicijalizirati objekt tipa MojaStruktura i pozvati njegovu metodu IspišiČlan koja treba ispisati "MojaStruktura".
-
+            var ms = new MojaStruktura("MojaStruktura");
+            ms.ispisiClan();
         }
 
         static void Main(string[] args)
