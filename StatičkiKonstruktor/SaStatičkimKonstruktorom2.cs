42cb528784fc6131d1f1e4e48609e64ac364c0f1
Senad Elezović <sen.elezovic@gmail.com>
2019-11-14 21:29:09 +0100

    Statički konstruktori

--- StatičkiKonstruktor/SaStatičkimKonstruktorom2.cs
+++ StatičkiKonstruktor/SaStatičkimKonstruktorom2.cs
@@ -4,11 +4,13 @@ namespace Vsite.CSharp.DefiniranjeTipa
 {
     class SaStatičkimKonstruktorom2 : SaStatičkimKonstruktorom1
     {
-        // TODO:094 Dodati prazni konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom2.KonstruktorKlase"
+        public SaStatičkimKonstruktorom2() {
+            Console.WriteLine("SaStatičkimKonstruktorom2.KonstruktorKlase");
+        }
+        static SaStatičkimKonstruktorom2() {
+            Console.WriteLine("SaStatičkimKonstruktorom2.StatičkiKonstruktor");
+        }
 
-        // TODO:095 Dodati statički konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom2.StatičkiKonstruktor"
-
-        // TODO:096 Ponovno pokrenuti program i pogledati ispis
 
         public void Metoda2()
         {
