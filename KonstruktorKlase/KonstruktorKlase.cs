94dd4c7927347bbb63230b219066cfeb40f7c7b9
Domagoj Krmpotić <domagojkrmpoti3@gmail.com>
2019-11-14 21:06:33 +0100

    Resolve constructor class

--- KonstruktorKlase/KonstruktorKlase.cs
+++ KonstruktorKlase/KonstruktorKlase.cs
@@ -6,13 +6,20 @@ namespace Vsite.CSharp.DefiniranjeTipa
     {
         public static void StvoriObjektPodrazumijevanimKonstruktorom()
         {
-            // TODO:060 Stvoriti objekt klase Osoba i ispisati vrijednosti članova Ime, Prezime, DatumRođenja
+            Osoba osoba1 = new Osoba("Jura", "Stublić",DateTime.MinValue);
+            Console.WriteLine(osoba1.Ime);
+            Console.WriteLine(osoba1.Prezime);
+            Console.WriteLine(osoba1.DatumRođenja);
+
 
         }
 
         public static void StvoriObjektZadanimKonstruktorom(string ime, string prezime, DateTime datumRođenja)
         {
-            // TODO:063 Stvoriti objekt klase Osoba pozivom tog konstruktora i ispisati vrijednosti članova Ime, Prezime, DatumRođenja
+            Osoba osoba = new Osoba(ime, prezime, datumRođenja);
+            Console.WriteLine(osoba.Ime);
+            Console.WriteLine(osoba.Prezime);
+            Console.WriteLine(osoba.DatumRođenja);
 
         }
 
