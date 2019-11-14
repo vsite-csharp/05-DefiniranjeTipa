0580d0f76799038b7bbda8f9c8b8fb156fcf9892
esvegot <esvegot@HR-00004162>
2019-11-14 20:44:06 +0100

    Resolve nested classes

--- UgnježđenaKlasa/UgnježđenaKlasa.cs
+++ UgnježđenaKlasa/UgnježđenaKlasa.cs
@@ -18,9 +18,9 @@ namespace Vsite.CSharp.DefiniranjeTipa
 
         public static void IzravnoKorištenjeUgnježđeneKlase()
         {
-            // TODO:042 Modificirati definicije tako da se klasa Ugnježđena može pozivati izravno:
-            //Gnijezdo.Ugnježđena gu = new Gnijezdo.Ugnježđena();
-            //gu.PredstaviSeNeposredno();
+            //  Modificirati definicije tako da se klasa Ugnježđena može pozivati izravno: Internal class Ugnježđena
+            Gnijezdo.Ugnježđena gu = new Gnijezdo.Ugnježđena();
+            gu.PredstaviSeNeposredno();
         }
     }
 }
