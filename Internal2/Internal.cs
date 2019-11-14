1bc74572dd9782c57b12742d24f5d7bbdedc5904
Ivan Blažeka <ivblazek@vsite.hr>
2019-11-14 20:17:13 +0100

    Resolve internal

--- Internal2/Internal.cs
+++ Internal2/Internal.cs
@@ -18,23 +18,20 @@ namespace Vsite.CSharp.DefiniranjeTipa
 
         public static void IspišiIzvedenuIzJavne()
         {
-            // TODO:030 Definiciju klase IzvedenaIzJavneBazne promijenite tako da je ona izvedena iz klase JavnaBazna.
             IzvedenaIzJavneBazne izvedenaIzJavne = new IzvedenaIzJavneBazne();
-            // TODO:031 Otkomentirajte donju naredbu, prevedite kod i provjerite radi li poziv ispravno.
-            //izvedenaIzJavne.IspišiMe();
+
+            izvedenaIzJavne.IspišiMe();
         }
 
         public static void IspišiIzvedenuIzBazne()
         {
-            // TODO:032 Definiciju klase IzvedenaIzBazne promijenite tako da je ona izvedena iz klase Bazna. Prevedite kod i popravite pogreške koje prevoditelj prijavljuje.
             IzvedenaIzBazne izvedenaIzBazne = new IzvedenaIzBazne();
-            // TODO:033 Otkomentirajte donju naredbu, prevedite kod i provjerite radi li poziv ispravno.
-            //izvedenaIzBazne.IspišiMe();
+
+            izvedenaIzBazne.IspišiMe();
         }
 
         public static void IspišiIzvedenuIzInterneBazne()
         {
-            // TODO:034 U klasi IzvedenaIzInterneBazneLokalne treba, bez promjene prava pristupa, omogućiti poziv metode IspišiMe iz klase InternaBazna ali bez promjene koda klase InternaBazna!
             IzvedenaIzInterneBazne izvedenaIzInterne = new IzvedenaIzInterneBazne();
             izvedenaIzInterne.IspišiMe();
         }
