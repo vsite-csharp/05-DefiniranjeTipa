6a921acbc66eccbedd71d822e7f4531e77d1f2ce
Marko Bohač <mabohac@vsite.hr>
2019-11-15 19:48:20 +0100

    Resolved struct constructor

--- KonstruktorStrukture/KonstruktorStrukture.cs
+++ KonstruktorStrukture/KonstruktorStrukture.cs
@@ -6,25 +6,23 @@ namespace Vsite.CSharp.DefiniranjeTipa
     {
         public static void PozivPodrazumijevanogKonstruktora()
         {
-            // TODO:070 Stvoriti objekt strukture Udaljenost i ispisati vrijednosti članova.
-
-
-            // TODO:072 Stvoriti objekt pozivom praznog konstruktora te ispisati vrijednosti članova.
-
-
+            Udaljenost u = new Udaljenost();
+            Console.WriteLine(u.Broj);
+            Console.WriteLine(u.Jedinica);
         }
 
         public static void PozivKonstruktoraSDvaArgumenta(double broj, JedinicaDuljine jedinica)
         {
-            // TODO:074 Stvoriti objekt pozivom konstruktora koji prima dva argumenta te ispisati vrijednosti članova.
-
-
+            Udaljenost u = new Udaljenost(broj, jedinica);
+            Console.WriteLine(u.Broj);
+            Console.WriteLine(u.Jedinica);
         }
 
         public static void PozivKonstruktoraSJednimArgumentom(double broj)
         {
-            // TODO:076 Stvoriti objekt pozivom konstruktora s jednim argumentom te ispisati vrijednosti članova.
-
+            Udaljenost u = new Udaljenost(broj);
+            Console.WriteLine(u.Broj);
+            Console.WriteLine(u.Jedinica);
         }
 
         static void Main(string[] args)
