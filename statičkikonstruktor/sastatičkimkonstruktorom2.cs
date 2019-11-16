3054be8011afc27e59cd927e0bc396d3ada15d34
Igor Obradovic <iobradovic@ZAWMIOB>
2019-11-16 17:52:32 +0100

    Remove old TODOs in *SaStatickimKonstruktorom2*.

---
+++ statičkikonstruktor/sastatičkimkonstruktorom2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Vsite.CSharp.DefiniranjeTipa
+{
+    class SaStatičkimKonstruktorom2 : SaStatičkimKonstruktorom1
+    {
+        public SaStatičkimKonstruktorom2()
+        {
+            Console.WriteLine("SaStatičkimKonstruktorom2.KonstruktorKlase");
+        }
+
+        static SaStatičkimKonstruktorom2()
+        {
+            Console.WriteLine("SaStatičkimKonstruktorom2.StatičkiKonstruktor");
+        }
+
+        public void Metoda2()
+        {
+            Console.WriteLine("SaStatičkimKonstruktorom2.Metoda2");
+        }
+    }
+}
