a004dc38a17ab3759b9218af1192935052af937b
Ivan Batinić <ivek81cro@gmail.com>
2019-11-20 13:39:13 +0100

    Resolved IDisposable

--- Destruktor/Destruktor.cs
+++ Destruktor/Destruktor.cs
@@ -11,7 +11,6 @@ namespace Vsite.CSharp.DefiniranjeTipa
                 KlasaSDestruktorom ksd = new KlasaSDestruktorom();
             }
 
-            // TODO:101 Dodati for petlju koja će se ponoviti 100000 puta i unutar koje će se kreirati novi objekti tipa KlasaSDestruktorom. Pokrenuti program i provjeriti redni broj prvog uništenog objekta.
             for(int i=0;i<=100000;++i)
             {
                 KlasaSDestruktorom klasa = new KlasaSDestruktorom();
