d4293a54093ca746ef49e52879906446ac1ab9b6
JosipVuckovic <josip.vuckovic@live.com>
2019-11-20 16:42:36 +0100

    Done all, tests passed, done late as laptop was in repair shop

--- Destruktor/Destruktor.cs
+++ Destruktor/Destruktor.cs
@@ -11,7 +11,10 @@ namespace Vsite.CSharp.DefiniranjeTipa
                 KlasaSDestruktorom ksd = new KlasaSDestruktorom();
             }
 
-            // TODO:101 Dodati for petlju koja će se ponoviti 100000 puta i unutar koje će se kreirati novi objekti tipa KlasaSDestruktorom. Pokrenuti program i provjeriti redni broj prvog uništenog objekta.
+            for (int i = 0; i < 100000; i++)
+            {
+                new KlasaSDestruktorom();
+            }
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey();
--- StatičkiKonstruktor/SaStatičkimKonstruktorom1.cs
+++ StatičkiKonstruktor/SaStatičkimKonstruktorom1.cs
@@ -4,11 +4,15 @@ namespace Vsite.CSharp.DefiniranjeTipa
 {
     class SaStatičkimKonstruktorom1
     {
-        // TODO:090 Dodati prazni konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom1.KonstruktorKlase"
+        public SaStatičkimKonstruktorom1()
+        {
+            Console.WriteLine("SaStatičkimKonstruktorom1.KonstruktorKlase");
+        }
 
-        // TODO:091 Dodati statički konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom1.StatičkiKonstruktor"
-
-        // TODO:092 Pokrenuti program i pogledati ispis
+        static SaStatičkimKonstruktorom1()
+        {
+            Console.WriteLine("SaStatičkimKonstruktorom1.StatičkiKonstruktor");
+        }
 
         public static void Metoda1()
         {
