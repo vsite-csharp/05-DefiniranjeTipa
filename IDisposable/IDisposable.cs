71f63802b55d90da95711b9464f20e97f0e1d141
KaMiletic <56977458+KaMiletic@users.noreply.github.com>
2019-11-21 19:54:02 +0100

    Resolve IDisposable

--- IDisposable/IDisposable.cs
+++ IDisposable/IDisposable.cs
@@ -7,13 +7,15 @@ namespace Vsite.CSharp.DefiniranjeTipa
     {
         public static void StvaranjeIPisanjeUDatoteku(string imeDatoteke)
         {
-            StreamWriter sw = new StreamWriter(imeDatoteke);
-            sw.WriteLine("Ovo je moj upis");
-            // TODO:120 Pokrenuti program i pogledati ispis.
+            using (StreamWriter sw = new StreamWriter(imeDatoteke))
+            {
+                sw.WriteLine("Ovo je moj upis");
+            }
+                
 
-            // TODO:121 Dodati poziv metode StreamWriter.Dispose (ili StreamWriter.Close) te ponovno pokrenuti program.
 
-            // TODO:122 Preraditi kod metode tako da se umjesto metode Dispose koristi blok using
+            //sw.Close() ili despose();
+            
 
         }
 
