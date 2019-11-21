61a059e16c75b5a59208c390e0965fc3ea3ca848
Marko Boroš <Marko.Boros@vsite.hr>
2019-11-21 19:42:21 +0100

    resolve gc.collect

--- Destruktor/Destruktor.cs
+++ Destruktor/Destruktor.cs
@@ -16,7 +16,6 @@ namespace Vsite.CSharp.DefiniranjeTipa
                     KlasaSDestruktorom ksd = new KlasaSDestruktorom();
                 }
             }
-            // TODO:101 Dodati for petlju koja će se ponoviti 100000 puta i unutar koje će se kreirati novi objekti tipa KlasaSDestruktorom. Pokrenuti program i provjeriti redni broj prvog uništenog objekta.
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey();
