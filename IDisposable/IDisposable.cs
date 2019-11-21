e4a69c6b69f571acf904f56de50ec71affe0d632
Krunoslav Bučar <krbuki@gmail.com>
2019-11-21 19:51:50 +0100

    ResolveIDisposable

--- IDisposable/IDisposable.cs
+++ IDisposable/IDisposable.cs
@@ -9,29 +9,33 @@ namespace Vsite.CSharp.DefiniranjeTipa
         {
             StreamWriter sw = new StreamWriter(imeDatoteke);
             sw.WriteLine("Ovo je moj upis");
-            // TODO:120 Pokrenuti program i pogledati ispis.
-                /*Usao sam u main
-                 * Konstruktor objekta br. 1
-                    Konstruktor objekta br. 2
-                    Konstruktor objekta br. 3
-                    Konstruktor objekta br. 4
-             *              .....
-             *      Destruktor objekta br. 985
-                    Destruktor objekta br. 984
-                    Destruktor objekta br. 983
-                    Destruktor objekta br. 982
-                    Destruktor objekta br. 981
-                    Destruktor objekta br. 980
-                    Destruktor objekta br. 979
-                    (stop)
-                     Destruktor objekta br. 1000 */
-            // TODO:121 Dodati poziv metode StreamWriter.Dispose (ili StreamWriter.Close) te ponovno pokrenuti program.
-            sw.Dispose();
-            // TODO:122 Preraditi kod metode tako da se umjesto metode Dispose koristi blok using
-            //using (StreamWriter ssw = new StreamWriter(imeDatoteke))
-            //{
-            //    sw.WriteLine("Ovo je 2. moj upis");
-            //}
+
+            /*Usao sam u main
+             * Konstruktor objekta br. 1
+                Konstruktor objekta br. 2
+                Konstruktor objekta br. 3
+                Konstruktor objekta br. 4
+         *              .....
+         *      Destruktor objekta br. 985
+                Destruktor objekta br. 984
+                Destruktor objekta br. 983
+                Destruktor objekta br. 982
+                Destruktor objekta br. 981
+                Destruktor objekta br. 980
+                Destruktor objekta br. 979
+                (stop)
+                 Destruktor objekta br. 1000 */
+           
+            //sw.Close(); //isto kao i sw.Dispose ili ili...pozivanje eksplicitno 
+            sw.Dispose();//Bez ovog neće moći obrisati datoteku  
+            
+
+            //osim eksplicitnog moguće je napraviti i ovako (dolje)
+            //možda bolje rješenje 
+            using (StreamWriter ssw = new StreamWriter(imeDatoteke))
+            {
+                sw.WriteLine("Ovo je 2. moj upis");
+            }
 
         }
 
