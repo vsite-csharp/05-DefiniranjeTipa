92767dc8e182941292032d8d9a05e4c3e3a24e75
Marko Boroš <Marko.Boros@vsite.hr>
2019-11-21 19:38:31 +0100

    Resolve destructor/finalize

--- Destruktor/Destruktor.cs
+++ Destruktor/Destruktor.cs
@@ -10,7 +10,12 @@ namespace Vsite.CSharp.DefiniranjeTipa
             {
                 KlasaSDestruktorom ksd = new KlasaSDestruktorom();
             }
-
+            for(int i = 0; i < 10000; ++i)
+            {
+                {
+                    KlasaSDestruktorom ksd = new KlasaSDestruktorom();
+                }
+            }
             // TODO:101 Dodati for petlju koja će se ponoviti 100000 puta i unutar koje će se kreirati novi objekti tipa KlasaSDestruktorom. Pokrenuti program i provjeriti redni broj prvog uništenog objekta.
 
             Console.WriteLine("GOTOVO!!!");
--- Destruktor/KlasaSDestruktorom.cs
+++ Destruktor/KlasaSDestruktorom.cs
@@ -14,8 +14,11 @@ namespace Vsite.CSharp.DefiniranjeTipa
             // dijagnostički ispis u Output prozor Visual Studija
             Debug.WriteLine(poruka);
         }
-
-        // TODO:100 Dodati destruktor (tj. finalizirajuću metodu) i njemu napisati naredbe koje će u konzolu i u Output prozor ispisati: "Destruktor objekta br. {RedniBroj}."
+        ~KlasaSDestruktorom()
+        {
+            Console.WriteLine($"Destruktor klase s destruktorom {RedniBroj}");
+            Debug.WriteLine($"Destruktor objekta br.{RedniBroj}");
+        }
 
 
         public readonly int RedniBroj; // redni broj objekta
