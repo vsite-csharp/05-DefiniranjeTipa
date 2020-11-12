5a0cd2b3315512425c695ff580eedb7702503fd0
Valentin Vareskic <valentin.vareskic@outlook.com>
2020-11-12 21:27:09 +0100

    Resolve definicije tipa

--- DefinicijaTipa/DefinicijaTipa.cs
+++ DefinicijaTipa/DefinicijaTipa.cs
@@ -4,19 +4,30 @@ namespace Vsite.CSharp.DefiniranjeTipa
 {
     class MojaKlasa
     {
-        // TODO:010 Dodati privatno polje (field - podatkovni član) "broj" tipa int i dodijeliti mu vrijednost 5
-
-        // TODO:011 Zadati javnu metodu IspišiČlan tipa void i u njoj dodati naredbu koja će ispisati podatkovni član "broj".
-
+        
+        private int broj = 5;
+        
+        public void IspišiČlan() { Console.WriteLine(broj); }
     }
 
 
     struct MojaStruktura
     {
-        // TODO:013 Dodati privatno polje (podatkovni član) "tekst" tipa string i dodijeliti mu vrijednost "MojaStruktura"
+       
 
-        // TODO:014 Zadati javnu metodu IspišiČlan tipa void i u njoj dodati naredbu koja će ispisati podatkovni član "tekst".
+       
 
+        public MojaStruktura(string tekst)
+        {
+            this.tekst = tekst;
+        }
+       
+        private string tekst;// = "MojaStruktura";
+       
+        public void IspišiČlan()
+        {
+            Console.WriteLine(tekst);
+        }
     }
 
 
@@ -24,17 +35,18 @@ namespace Vsite.CSharp.DefiniranjeTipa
     {
         public static void IspišiČlanKlase()
         {
-            // TODO:012 Inicijalizirati objekt tipa MojaKlasa i pozvati njegovu metodu IspišiČlan
-
+            
+            MojaKlasa mk = new MojaKlasa();
+            mk.IspišiČlan();
         }
 
         public static void IspišiČlanStrukture()
         {
-            // TODO:015 Inicijalizirati objekt tipa MojaStruktura i pozvati njegovu metodu IspišiČlan koja treba ispisati "MojaStruktura".
-
+            MojaStruktura ms = new MojaStruktura("MojaStruktura");
+            ms.IspišiČlan();
         }
 
-        // TODO:016 Pokrenuti testove i provjeriti prolaze li oba testa u grupi TestDefinicijeTipa.
+        
 
         static void Main(string[] args)
         {
