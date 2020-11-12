2cd5cbb2aa6e78cce9505548b6c3be20cae1b8f2
Ivan Mihin <56129012+Ivan-Mihin@users.noreply.github.com>
2020-11-12 20:59:34 +0100

    Resolve using objects

--- KorištenjeObjekata/KorištenjeObjekata.cs
+++ KorištenjeObjekata/KorištenjeObjekata.cs
@@ -6,37 +6,42 @@ namespace Vsite.CSharp.DefiniranjeTipa
 {
     class KorištenjeObjekata
     {
-        // TODO:000 Ispisati trenutni datum i vrijeme pomoću statičkog svojstva DateTime.Now
+        // Ispisati trenutni datum i vrijeme pomoću statičkog svojstva DateTime.Now
         public static void IspišiTrenutniDatumVrijeme()
         {
-
+            var sad = DateTime.Now;
+            Console.WriteLine(sad);
         }
 
-        // TODO:001 Ispisati današnji datum pomoću statičkog svojstva DateTime.Now i metode DateTime.ToShortDateString
+        // Ispisati današnji datum pomoću statičkog svojstva DateTime.Now i metode DateTime.ToShortDateString
         public static void IspišiDanašnjiDatum()
         {
-
+            var sad = DateTime.Now;
+            Console.WriteLine(sad.ToShortDateString());
         }
 
-        // TODO:002 Ispisati trenutno vrijeme pomoću statičkog svojstva DateTime.Now i metode DateTime.ToShortTimeString
+        // Ispisati trenutno vrijeme pomoću statičkog svojstva DateTime.Now i metode DateTime.ToShortTimeString
         public static void IspišiTrenutnoVrijeme()
         {
-
+            Console.WriteLine(DateTime.Now.ToShortTimeString());
         }
 
         public static int IzračunajBrojDanaIzmeđuDvaDatuma(int godina1, int mjesec1, int dan1, int godina2, int mjesec2, int dan2)
         {
-            // TODO:003 Stvoriti objekt "datum1" tipa DateTime tako da se njegovom konstruktoru proslijede godina1, mjesec1 i dan1.
+            // Stvoriti objekt "datum1" tipa DateTime tako da se njegovom konstruktoru proslijede godina1, mjesec1 i dan1.
+            var datum1 = new DateTime(godina1, mjesec1, dan1);
 
-            // TODO:004 Stvoriti objekt "datum2" tipa DateTime tako da se njegovom konstruktoru proslijede godina2, mjesec2 i dan2.
+            // Stvoriti objekt "datum2" tipa DateTime tako da se njegovom konstruktoru proslijede godina2, mjesec2 i dan2.
+            var datum2 = new DateTime(godina2, mjesec2, dan2);
 
-            // TODO:005 Izračunati razliku između datum1 i datum2 (jednostavnim oduzimanjem: datum2 - datum1) i tu razliku dodijeliti varijabli "period" tipa TimeSpan (koja služi za pohranu vremenskog perioda).
+            // Izračunati razliku između datum1 i datum2 (jednostavnim oduzimanjem: datum2 - datum1) i tu razliku dodijeliti varijabli "period" tipa TimeSpan (koja služi za pohranu vremenskog perioda).
+            var period = datum2 - datum1;
 
-            // TODO:006 Budući da metoda mora kao rezultat vratiti izračunati period izražen kao broj dana, iz prethodno izračunatog perioda treba dohvatiti i vratiti svojstvo Days (tj. svojstvo TimeSpan.Days).
-            return 0;
+            // Budući da metoda mora kao rezultat vratiti izračunati period izražen kao broj dana, iz prethodno izračunatog perioda treba dohvatiti i vratiti svojstvo Days (tj. svojstvo TimeSpan.Days).
+            return period.Days;
         }
 
-        // TODO:007 Pokrenuti testove i provjeriti da svih 5 testova u grupi TestKorištenjaObjekta prolaze
+        // Pokrenuti testove i provjeriti da svih 5 testova u grupi TestKorištenjaObjekta prolaze
         static void Main(string[] args)
         {
             Thread.CurrentThread.CurrentCulture = new CultureInfo("hr");
