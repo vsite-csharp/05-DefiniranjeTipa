747a10ba390ac236155cdd3a58f003bdbcedbda4
jsribar <julijan.sribar@gmail.com>
2020-11-18 21:55:39 +0100

    Correct TODO description

--- Internal2/Internal.cs
+++ Internal2/Internal.cs
@@ -27,19 +27,20 @@ namespace Vsite.CSharp.DefiniranjeTipa
         public static void IspišiIzvedenuIzBazne()
         {
             // TODO:032 Definiciju klase IzvedenaIzBazne promijenite tako da je ona izvedena iz klase Bazna. Prevedite kod i popravite pogreške koje prevoditelj prijavljuje.
+            // TODO:033 Promijenite pravo pristupa klasi Bazna da se kod može prevesti.
             IzvedenaIzBazne izvedenaIzBazne = new IzvedenaIzBazne();
-            // TODO:033 Otkomentirajte donju naredbu, prevedite kod i provjerite radi li poziv ispravno.
+            // TODO:034 Otkomentirajte donju naredbu, prevedite kod i provjerite radi li poziv ispravno.
             //izvedenaIzBazne.IspišiMe();
         }
 
         public static void IspišiIzvedenuIzInterneBazne()
         {
-            // TODO:034 U klasi IzvedenaIzInterneBazne treba, bez promjene prava pristupa, omogućiti poziv metode IspišiMe iz klase InternaBazna ali bez promjene koda klase InternaBazna!
+            // TODO:035 U klasi IzvedenaIzInterneBazne treba, bez promjene prava pristupa, omogućiti poziv metode IspišiMe iz klase InternaBazna ali bez promjene koda klase InternaBazna!
             IzvedenaIzInterneBazne izvedenaIzInterne = new IzvedenaIzInterneBazne();
-            // TODO:035 Otkomentirati donju naredbu i provjeriti izvođenje.
+            // TODO:036 Otkomentirati donju naredbu i provjeriti izvođenje.
             //izvedenaIzInterne.IspišiMe();
         }
 
-        // TODO:036 Pokrenuti testove i provjeriti prolaze li sva 3 testa u grupi TestInternal
+        // TODO:037 Pokrenuti testove i provjeriti prolaze li sva 3 testa u grupi TestInternal
     }
 }
