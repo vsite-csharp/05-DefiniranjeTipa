e00b44e53cfb5600798c5b5af1217e0dd11a1033
jsribar <julijan.sribar@gmail.com>
2020-11-18 21:55:39 +0100

    Correct TODO description

--- IDisposable/IDisposable.cs
+++ IDisposable/IDisposable.cs
@@ -3,26 +3,46 @@ using System.IO;
 
 namespace Vsite.CSharp.DefiniranjeTipa
 {
+    // TODO:122 Deklarirati da klasa implementira IDisposable sučelje te dodati javnu metodu Dispose.
+    // TODO:123 Dodati u klasu metodu protected virtual void Dispose(bool disposing) i u nju prebaciti poziv StreamWriter.Close iz destruktora.
+    // TODO:124 Napraviti pozive metode iz destruktora i iz javne metode Dispose.
+    class RadSdatotekom
+    {
+        private StreamWriter sw;
+
+        public RadSdatotekom(string imeDatoteke)
+        {
+            sw = new StreamWriter(imeDatoteke);
+        }
+
+        public void Zapiši(string tekst)
+        {
+            sw.Write(tekst);
+        }
+
+        // TODO:121 Dodati klasi destruktor koji će pozvati metodu StreamWriter.Close (odn. StreamWriter.Dispose). Pokrenuti program i provjeriti ispis.
+    }
+
     class Disposable
     {
         public static void StvaranjeIPisanjeUDatoteku(string imeDatoteke)
         {
-            StreamWriter sw = new StreamWriter(imeDatoteke);
-            sw.WriteLine("Ovo je moj upis");
-            // TODO:120 Pokrenuti program i pogledati ispis.
+            RadSdatotekom rd = new RadSdatotekom(imeDatoteke);
+            rd.Zapiši("Ovo je moj upis");
 
-            // TODO:121 Dodati poziv metode StreamWriter.Dispose (ili StreamWriter.Close) te ponovno pokrenuti program.
+            // TODO:125 Dodati poziv metode RadSDatotekom.Dispose.
 
-            // TODO:122 Preraditi kod metode tako da se umjesto metode Dispose koristi blok using
+            // TODO:127 Preraditi kod metode tako da se umjesto metode Dispose koristi blok using.
 
         }
 
+        // TODO:126 Staviti točku prekida (breakpoint) na poziv metode BrisanjeDatoteke, pokrenuti program i provjeriti sadržaj datoteke prije brisanja.
         public static void BrisanjeDatoteke(string imeDatoteke)
         {
             File.Delete(imeDatoteke);
         }
 
-
+        // TODO:120 Pokrenuti program i pogledati ispis. Provjeriti sadržaj datoteke "moj.txt".
         static void Main(string[] args)
         {
             try
