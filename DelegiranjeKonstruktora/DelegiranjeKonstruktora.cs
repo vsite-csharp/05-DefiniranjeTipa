54c79cc987ff98d81698c8caa8fbf8f7ef3ee673
Klara Blašković <klblasko@vsite.hr>
2020-11-19 20:49:49 +0100

    Resolve constructor delegation

--- DelegiranjeKonstruktora/DelegiranjeKonstruktora.cs
+++ DelegiranjeKonstruktora/DelegiranjeKonstruktora.cs
@@ -7,21 +7,25 @@ namespace Vsite.CSharp.DefiniranjeTipa
 
         public static void StvoriInstancuIzvedeneKlaseKonstruktoromSDvaArgumenta(int a, int b)
         {
-            // TODO:082 Stvoriti objekt pozivom tog konstruktora:
-            //Izvedena i = new Izvedena(a, b);
+            // Stvoriti objekt pozivom tog konstruktora:
+            Izvedena i = new Izvedena(a, b);
 
-            // TODO:083 Dodati dvije naredbe koje će ispisati vrijednosti članova A i B stvorenog objekta i, pokrenuti kod i provjeriti ispis.
+            // Dodati dvije naredbe koje će ispisati vrijednosti članova A i B stvorenog objekta i, pokrenuti kod i provjeriti ispis.
+            Console.WriteLine(i.A);
+            Console.WriteLine(i.B);
         }
 
         public static void StvoriInstancuIzvedeneKlaseKonstruktoromSJednimArgumentom(int a)
         {
-            // TODO:086 Stvoriti objekt pozivom tog konstruktora:
-            // Izvedena i = new Izvedena(a);
+            // Stvoriti objekt pozivom tog konstruktora:
+            Izvedena i = new Izvedena(a);
 
-            // TODO:087 Dodati dvije naredbe koje će ispisati vrijednosti članova A i B stvorenog objekta i, pokrenuti kod i provjeriti ispis.
+            // Dodati dvije naredbe koje će ispisati vrijednosti članova A i B stvorenog objekta i, pokrenuti kod i provjeriti ispis.
+            Console.WriteLine(i.A);
+            Console.WriteLine(i.B);
         }
 
-        // TODO:088 Pokrenuti testove i provjeriti da oba testa u grupi TestDelegiranjaKonstruktora prolaze.
+        // Pokrenuti testove i provjeriti da oba testa u grupi TestDelegiranjaKonstruktora prolaze.
         static void Main(string[] args)
         {
             StvoriInstancuIzvedeneKlaseKonstruktoromSDvaArgumenta(5, 7);
