f80c74b9a03e0993bd87e2224e8903f494efe170
Valentina Behin <72219767+ValentinaBehin@users.noreply.github.com>
2020-11-19 19:23:49 +0100

    Resolve difference between class and structure

--- Class-Struct/Class-Struct.cs
+++ Class-Struct/Class-Struct.cs
@@ -2,32 +2,49 @@
 
 namespace Vsite.CSharp.DefiniranjeTipa
 {
-    // TODO:020 Prekopirati kod strukture iz prethodnog primjera i promijeniti pravo pristupa članu "tekst" u public.
+    // Prekopirati kod strukture iz prethodnog primjera i promijeniti pravo pristupa članu "tekst" u public.
     struct MojaStruktura
     {
-
+        public MojaStruktura(string tekst)
+        {
+            this.tekst = tekst;
+        }
+        public string tekst;
+        public void ispišiČlan()
+        {
+            Console.WriteLine(tekst);
+        }
 
     }
 
-    // TODO:025 Prekopirati kod klase iz prethodnog primjera i promijeniti pravo pristupa članu "broj" u public.
+    //  Prekopirati kod klase iz prethodnog primjera i promijeniti pravo pristupa članu "broj" u public.
     class MojaKlasa
     {
+        public int broj = 5;
+        public void ispišiČlan()
+        {
 
+            Console.WriteLine(broj);
+        }
     }
 
     class ClassStruct
     {
         public static void IspišiStrukture()
         {
-            // TODO:021 Promijeniti poziv konstruktora tako da ispis bude "MojaStruktura".
-            MojaStruktura ms1 = new MojaStruktura();
+            //  Promijeniti poziv konstruktora tako da ispis bude "MojaStruktura".
+            MojaStruktura ms1 = new MojaStruktura("MojaStruktura");
             MojaStruktura ms2 = ms1;
-            // TODO:022 Pozvati metodu IspišiČlan za obje instance (ms1 i ms2).
+            //  Pozvati metodu IspišiČlan za obje instance (ms1 i ms2).
+            ms1.ispišiČlan();
+            ms2.ispišiČlan();
 
             Console.WriteLine();
-            // TODO:023 Otkomentirati donju naredbu.
-            //ms2.tekst = "MyStruct";
-            // TODO:024 Ponovno pozvati metodu IspišiČlan za obje instance te obrazložiti rezultat.
+            //  Otkomentirati donju naredbu.
+            ms2.tekst = "mystruct";
+            //  Ponovno pozvati metodu IspišiČlan za obje instance te obrazložiti rezultat.
+            ms1.ispišiČlan();
+            ms2.ispišiČlan();//-mijenja se vrijednost
 
         }
 
@@ -35,16 +52,19 @@ namespace Vsite.CSharp.DefiniranjeTipa
         {
             MojaKlasa mk1 = new MojaKlasa();
             MojaKlasa mk2 = mk1;
-            // TODO:026 Napisati naredbe koje pozivaju metodu IspišiČlan za obje instance.
-
+            //  Napisati naredbe koje pozivaju metodu IspišiČlan za obje instance.
+            mk1.ispišiČlan();
+            mk2.ispišiČlan();
             Console.WriteLine();
-            // TODO:027 Otkomentirati donju naredbu.
-            //mk2.broj = 2;
-            // TODO:028 Ponovno pozvati metodu IspišiČlan za obje instance te obrazložiti rezultat.
+            //  Otkomentirati donju naredbu.
+            mk2.broj = 2;
+            //  Ponovno pozvati metodu IspišiČlan za obje instance te obrazložiti rezultat.
+            mk1.ispišiČlan();
+            mk2.ispišiČlan();//ispis 2 2
 
         }
 
-        // TODO:029 Pokrenuti testove i provjeriti prolaze li oba testa u grupi TestClass_Struct.
+        //  Pokrenuti testove i provjeriti prolaze li oba testa u grupi TestClass_Struct.
 
         static void Main(string[] args)
         {
