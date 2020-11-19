deafcf973624f3bd39f4237100f75fa6c9ca323c
dajergov <dadoj@DESKTOP-4PTLH1S>
2020-11-19 19:39:37 +0100

    Resolve internal class inheritance

--- Internal2/Internal.cs
+++ Internal2/Internal.cs
@@ -18,28 +18,28 @@ namespace Vsite.CSharp.DefiniranjeTipa
 
         public static void IspišiIzvedenuIzJavne()
         {
-            // TODO:030 Definiciju klase IzvedenaIzJavneBazne promijenite tako da je ona izvedena iz klase JavnaBazna.
+            // Definiciju klase IzvedenaIzJavneBazne promijenite tako da je ona izvedena iz klase JavnaBazna.
             IzvedenaIzJavneBazne izvedenaIzJavne = new IzvedenaIzJavneBazne();
-            // TODO:031 Otkomentirajte donju naredbu, prevedite kod i provjerite radi li poziv ispravno.
-            //izvedenaIzJavne.IspišiMe();
+            // Otkomentirajte donju naredbu, prevedite kod i provjerite radi li poziv ispravno.
+            izvedenaIzJavne.IspišiMe();
         }
 
         public static void IspišiIzvedenuIzBazne()
         {
-            // TODO:032 Definiciju klase IzvedenaIzBazne promijenite tako da je ona izvedena iz klase Bazna. Prevedite kod i popravite pogreške koje prevoditelj prijavljuje.
+            // Definiciju klase IzvedenaIzBazne promijenite tako da je ona izvedena iz klase Bazna. Prevedite kod i popravite pogreške koje prevoditelj prijavljuje.
             IzvedenaIzBazne izvedenaIzBazne = new IzvedenaIzBazne();
-            // TODO:033 Otkomentirajte donju naredbu, prevedite kod i provjerite radi li poziv ispravno.
-            //izvedenaIzBazne.IspišiMe();
+            // Otkomentirajte donju naredbu, prevedite kod i provjerite radi li poziv ispravno.
+            izvedenaIzBazne.IspišiMe();
         }
 
         public static void IspišiIzvedenuIzInterneBazne()
         {
-            // TODO:034 U klasi IzvedenaIzInterneBazne treba, bez promjene prava pristupa, omogućiti poziv metode IspišiMe iz klase InternaBazna ali bez promjene koda klase InternaBazna!
+            // U klasi IzvedenaIzInterneBazne treba, bez promjene prava pristupa, omogućiti poziv metode IspišiMe iz klase InternaBazna ali bez promjene koda klase InternaBazna!
             IzvedenaIzInterneBazne izvedenaIzInterne = new IzvedenaIzInterneBazne();
-            // TODO:035 Otkomentirati donju naredbu i provjeriti izvođenje.
-            //izvedenaIzInterne.IspišiMe();
+            // Otkomentirati donju naredbu i provjeriti izvođenje.
+            izvedenaIzInterne.IspišiMe();
         }
 
-        // TODO:036 Pokrenuti testove i provjeriti prolaze li sva 3 testa u grupi TestInternal
+        // Pokrenuti testove i provjeriti prolaze li sva 3 testa u grupi TestInternal
     }
 }
