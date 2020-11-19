bdc84c623fe229ae36fd7af7a0066abf26e5c204
Kristian Petretić <krpetret@vsite.hr>
2020-11-19 19:28:13 +0100

    resolve class and structure

--- Class-Struct/Class-Struct.cs
+++ Class-Struct/Class-Struct.cs
@@ -5,14 +5,20 @@ namespace Vsite.CSharp.DefiniranjeTipa
     // TODO:020 Prekopirati kod strukture iz prethodnog primjera i promijeniti pravo pristupa članu "tekst" u public.
     struct MojaStruktura
     {
+        
 
 
     }
 
-    // TODO:025 Prekopirati kod klase iz prethodnog primjera i promijeniti pravo pristupa članu "broj" u public.
+    //  Prekopirati kod klase iz prethodnog primjera i promijeniti pravo pristupa članu "broj" u public.
     class MojaKlasa
     {
-
+        public int broj = 5;
+        //  Zadati javnu metodu IspišiČlan tipa void i u njoj dodati naredbu koja će ispisati podatkovni član "broj".
+        public void ispisiClan()
+        {
+            Console.WriteLine(broj);
+        }
     }
 
     class ClassStruct
@@ -35,12 +41,15 @@ namespace Vsite.CSharp.DefiniranjeTipa
         {
             MojaKlasa mk1 = new MojaKlasa();
             MojaKlasa mk2 = mk1;
-            // TODO:026 Napisati naredbe koje pozivaju metodu IspišiČlan za obje instance.
+            //  Napisati naredbe koje pozivaju metodu IspišiČlan za obje instance.
 
             Console.WriteLine();
-            // TODO:027 Otkomentirati donju naredbu.
-            //mk2.broj = 2;
-            // TODO:028 Ponovno pozvati metodu IspišiČlan za obje instance te obrazložiti rezultat.
+            mk1.ispisiClan();
+            mk2.ispisiClan();
+
+            //  Otkomentirati donju naredbu.
+            mk2.broj = 2;
+            //  Ponovno pozvati metodu IspišiČlan za obje instance te obrazložiti rezultat.
 
         }
 
