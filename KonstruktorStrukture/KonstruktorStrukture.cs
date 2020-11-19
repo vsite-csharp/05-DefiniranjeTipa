7a61b7d1b10aabadbbda175473c520d0a7185975
Ivan Posavac <ivposava@vsite.hr>
2020-11-19 20:23:51 +0100

    Resolve structure constructor

--- KonstruktorStrukture/KonstruktorStrukture.cs
+++ KonstruktorStrukture/KonstruktorStrukture.cs
@@ -6,28 +6,33 @@ namespace Vsite.CSharp.DefiniranjeTipa
     {
         public static void PozivPodrazumijevanogKonstruktora()
         {
-            // TODO:070 Stvoriti objekt strukture Udaljenost i ispisati vrijednosti članova.
-
-
-            // TODO:072 Stvoriti objekt pozivom praznog konstruktora te ispisati vrijednosti članova.
+            // Stvoriti objekt strukture Udaljenost i ispisati vrijednosti članova.
+            var d = new Udaljenost();
+            Console.WriteLine(d.Broj);
+            Console.WriteLine(d.Jedinica);
+            //  Stvoriti objekt pozivom praznog konstruktora te ispisati vrijednosti članova.
 
 
         }
 
         public static void PozivKonstruktoraSDvaArgumenta(double broj, JedinicaDuljine jedinica)
         {
-            // TODO:074 Stvoriti objekt pozivom konstruktora koji prima dva argumenta te ispisati vrijednosti članova.
-
+            // Stvoriti objekt pozivom konstruktora koji prima dva argumenta te ispisati vrijednosti članova.
+            var d = new Udaljenost(broj, jedinica);
+            Console.WriteLine(d.Broj);
+            Console.WriteLine(d.Jedinica);
 
         }
 
         public static void PozivKonstruktoraSJednimArgumentom(double broj)
         {
-            // TODO:076 Stvoriti objekt pozivom konstruktora s jednim argumentom te ispisati vrijednosti članova.
-
+            // Stvoriti objekt pozivom konstruktora s jednim argumentom te ispisati vrijednosti članova.
+            var d = new Udaljenost(broj);
+            Console.WriteLine(d.Broj);
+            Console.WriteLine(d.Jedinica);
         }
 
-        // TODO:077 Pokrenuti testove i provjeriti prolaze li sva 3 testa u grupi TestKonstruktoraStrukture
+        // Pokrenuti testove i provjeriti prolaze li sva 3 testa u grupi TestKonstruktoraStrukture
 
         static void Main(string[] args)
         {
