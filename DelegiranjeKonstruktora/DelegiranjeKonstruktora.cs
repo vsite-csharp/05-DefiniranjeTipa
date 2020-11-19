451e9557564949857eb5b56588a266b84349b503
Kristian Petretić <krpetret@vsite.hr>
2020-11-19 21:07:32 +0100

    resolve static constructor

--- DelegiranjeKonstruktora/DelegiranjeKonstruktora.cs
+++ DelegiranjeKonstruktora/DelegiranjeKonstruktora.cs
@@ -18,14 +18,14 @@ namespace Vsite.CSharp.DefiniranjeTipa
 
         public static void StvoriInstancuIzvedeneKlaseKonstruktoromSJednimArgumentom(int a)
         {
-            // TODO:086 Stvoriti objekt pozivom tog konstruktora:
+            //  Stvoriti objekt pozivom tog konstruktora:
              Izvedena i = new Izvedena(a);
             //Console.WriteLine(i.a);
             //Console.WriteLine(i.b);
-            // TODO:087 Dodati dvije naredbe koje će ispisati vrijednosti članova A i B stvorenog objekta i, pokrenuti kod i provjeriti ispis.
+            //  Dodati dvije naredbe koje će ispisati vrijednosti članova A i B stvorenog objekta i, pokrenuti kod i provjeriti ispis.
         }
 
-        // TODO:088 Pokrenuti testove i provjeriti da oba testa u grupi TestDelegiranjaKonstruktora prolaze.
+        //  Pokrenuti testove i provjeriti da oba testa u grupi TestDelegiranjaKonstruktora prolaze.
         static void Main(string[] args)
         {
             StvoriInstancuIzvedeneKlaseKonstruktoromSDvaArgumenta(5, 7);
