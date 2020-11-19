db9670a3d6ec1919b926ada12c964457a6e23fa2
Anamarija Jozić <anjozic@vsite.hr>
2020-11-19 20:49:52 +0100

    Resolve constructor delegation

--- DelegiranjeKonstruktora/DelegiranjeKonstruktora.cs
+++ DelegiranjeKonstruktora/DelegiranjeKonstruktora.cs
@@ -9,7 +9,7 @@ namespace Vsite.CSharp.DefiniranjeTipa
         {
             // TODO:082 Stvoriti objekt pozivom tog konstruktora:
             //Izvedena i = new Izvedena(a, b);
-
+            //
             // TODO:083 Dodati dvije naredbe koje će ispisati vrijednosti članova A i B stvorenog objekta i, pokrenuti kod i provjeriti ispis.
         }
 
@@ -21,7 +21,7 @@ namespace Vsite.CSharp.DefiniranjeTipa
             // TODO:087 Dodati dvije naredbe koje će ispisati vrijednosti članova A i B stvorenog objekta i, pokrenuti kod i provjeriti ispis.
         }
 
-        // TODO:088 Pokrenuti testove i provjeriti da oba testa u grupi TestDelegiranjaKonstruktora prolaze.
+        // Pokrenuti testove i provjeriti da oba testa u grupi TestDelegiranjaKonstruktora prolaze.
         static void Main(string[] args)
         {
             StvoriInstancuIzvedeneKlaseKonstruktoromSDvaArgumenta(5, 7);
