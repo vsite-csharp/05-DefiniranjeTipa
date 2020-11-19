578ee31dc23ad458998d54a80b67256c48cb1840
Tomislav Peša <72340847+Topesa@users.noreply.github.com>
2020-11-19 19:39:42 +0100

    Resolve internal

--- Class-Struct/Class-Struct.cs
+++ Class-Struct/Class-Struct.cs
@@ -9,21 +9,18 @@ namespace Vsite.CSharp.DefiniranjeTipa
         {
             this.tekst = tekst;
         }
-        //  Dodati privatno polje (podatkovni član) "tekst" tipa string i dodijeliti mu vrijednost "MojaStruktura"
-        private string tekst;// = "MojaStruktura";
-        //  Zadati javnu metodu IspišiČlan tipa void i u njoj dodati naredbu koja će ispisati podatkovni član "tekst".
+        public string tekst;
         public void IspišiČlan()
         {
             Console.WriteLine(tekst);
         }
+
     }
 
-    // TODO:025 Prekopirati kod klase iz prethodnog primjera i promijeniti pravo pristupa članu "broj" u public.
+    // Prekopirati kod klase iz prethodnog primjera i promijeniti pravo pristupa članu "broj" u public.
     class MojaKlasa
     {
-        // Dodati privatno polje (field - podatkovni član) "broj" tipa int i dodijeliti mu vrijednost 5
         public int broj = 5;
-        //  Zadati javnu metodu IspišiČlan tipa void i u njoj dodati naredbu koja će ispisati podatkovni član "broj".
         public void IspišiČlan()
         {
             Console.WriteLine(broj);
@@ -44,28 +41,28 @@ namespace Vsite.CSharp.DefiniranjeTipa
             Console.WriteLine();
             // Otkomentirati donju naredbu.
             ms2.tekst = "MyStruct";
-            // TODO:024 Ponovno pozvati metodu IspišiČlan za obje instance te obrazložiti rezultat.
+            // Ponovno pozvati metodu IspišiČlan za obje instance te obrazložiti rezultat.
             ms1.IspišiČlan();
             ms2.IspišiČlan();
-
         }
+
 
         public static void IspišiKlase()
         {
             MojaKlasa mk1 = new MojaKlasa();
             MojaKlasa mk2 = mk1;
-            // TODO:026 Napisati naredbe koje pozivaju metodu IspišiČlan za obje instance.
+            //  Napisati naredbe koje pozivaju metodu IspišiČlan za obje instance.
             mk1.IspišiČlan();
             mk2.IspišiČlan();
 
             Console.WriteLine();
-            // TODO:027 Otkomentirati donju naredbu.
+            // Otkomentirati donju naredbu.
             mk2.broj = 2;
-            // TODO:028 Ponovno pozvati metodu IspišiČlan za obje instance te obrazložiti rezultat.
+            // Ponovno pozvati metodu IspišiČlan za obje instance te obrazložiti rezultat.
 
         }
 
-        // TODO:029 Pokrenuti testove i provjeriti prolaze li oba testa u grupi TestClass_Struct.
+        // Pokrenuti testove i provjeriti prolaze li oba testa u grupi TestClass_Struct.
 
         static void Main(string[] args)
         {
