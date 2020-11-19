b8b5664ef7220914c0fdb89c3b51a96ff17d0858
dajergov <dadoj@DESKTOP-4PTLH1S>
2020-11-19 20:01:04 +0100

    Resolve static members

--- StatičkiČlanovi/BrojačInstanci.cs
+++ StatičkiČlanovi/BrojačInstanci.cs
@@ -4,29 +4,30 @@ namespace Vsite.CSharp.StatičkiČlanovi
 {
     class BrojačInstanci
     {
-        // TODO:050 Dodati privatni statički član "brojač" tipa int i inicijalizirati ga na 0
+        // Dodati privatni statički član "brojač" tipa int i inicijalizirati ga na 0
+        private static int brojač = 0;
 
-
-        // TODO:051 U metodu dodati naredbu koja će ispisati trenutnu vrijednost člana brojač
+        // U metodu dodati naredbu koja će ispisati trenutnu vrijednost člana brojač
         public static void IspišiBrojStvorenihObjekata()
         {
-
+            Console.WriteLine(brojač);
         }
 
-        // TODO:052 U klasu dodati privatni član "instanca" tipa int
+        // U klasu dodati privatni član "instanca" tipa int
+        private int instanca = 0;
 
-        // TODO:053 U konstruktor dodati naredbu koja će uvećati "brojač" za 1 i uvećanu vrijednost dodijeliti članu "instanca"
+        // U konstruktor dodati naredbu koja će uvećati "brojač" za 1 i uvećanu vrijednost dodijeliti članu "instanca"
         public BrojačInstanci()
         {
-
+            instanca = ++brojač;
         }
 
-        // TODO:054 U metodu dodati naredbu koja će ispisati vrijednost člana "instanca"
+        // U metodu dodati naredbu koja će ispisati vrijednost člana "instanca"
         public void IspišiRedniBrojObjekta()
         {
-
+            Console.WriteLine(instanca);
         }
 
-        // TODO:055 Pokrenuti testove i provjeriti prolaze li oba testa u grupi TestStatičkihČlanova
+        // Pokrenuti testove i provjeriti prolaze li oba testa u grupi TestStatičkihČlanova
     }
 }
