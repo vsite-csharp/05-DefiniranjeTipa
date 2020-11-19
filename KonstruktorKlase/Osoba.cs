d8443a94358c4d6370061cf7f4bc2ffe8d53eaf4
Ante Žunić <anzunic@vsite.hr>
2020-11-19 20:13:19 +0100

    Resolve class constructor

--- KonstruktorKlase/Osoba.cs
+++ KonstruktorKlase/Osoba.cs
@@ -8,8 +8,19 @@ namespace Vsite.CSharp.DefiniranjeTipa
         public readonly string Prezime;
         public readonly DateTime DatumRođenja;
 
-        // TODO:061 Dodati prazni konstruktor koji inicijalizira članove objekta na neku vrijednost, ponovno pokrenuti program i provjeriti ispis.
-
-        // TODO:062 Promijeniti prazan konstruktor tako da prima 3 argumenta (ime, prezime, datumRođenja) kojima se inicijaliziraju članovi objekta.
+        // Dodati prazni konstruktor koji inicijalizira članove objekta na neku vrijednost, ponovno pokrenuti program i provjeriti ispis.
+        public Osoba()
+        {
+        //    Ime = "pero";
+        //    Prezime = "Horvat";
+        //    DatumRođenja = new DateTime(1985, 3, 21);
+        }
+        // Promijeniti prazan konstruktor tako da prima 3 argumenta (ime, prezime, datumRođenja) kojima se inicijaliziraju članovi objekta.
+        public Osoba(string ime, string prezime, DateTime datum)
+        {
+            Ime = ime;
+            Prezime = prezime;
+            DatumRođenja = datum;
+        }
     }
 }
