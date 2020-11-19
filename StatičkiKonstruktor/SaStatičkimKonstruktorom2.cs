324cb067a1514b17d61280debfcc848a384c2524
Filip Lončar <filoncar@vsite.hr>
2020-11-19 21:03:05 +0100

    Resolve static constructor

--- StatičkiKonstruktor/SaStatičkimKonstruktorom2.cs
+++ StatičkiKonstruktor/SaStatičkimKonstruktorom2.cs
@@ -4,13 +4,19 @@ namespace Vsite.CSharp.DefiniranjeTipa
 {
     class SaStatičkimKonstruktorom2 : SaStatičkimKonstruktorom1
     {
-        // TODO:094 Dodati prazni konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom2.KonstruktorKlase"
+        // ++TODO:094 Dodati prazni konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom2.KonstruktorKlase"
+        public SaStatičkimKonstruktorom2()
+        {
+            Console.WriteLine("SaStatičkimKonstruktorom2.KonstruktorKlase");
+        }
+        // ++TODO:095 Dodati statički konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom2.StatičkiKonstruktor"
+        static SaStatičkimKonstruktorom2()
+        {
+            Console.WriteLine("SaStatičkimKonstruktorom2.StatičkiKonstruktor");
+        }
+        // ++TODO:096 Ponovno pokrenuti program i pogledati ispis
 
-        // TODO:095 Dodati statički konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom2.StatičkiKonstruktor"
-
-        // TODO:096 Ponovno pokrenuti program i pogledati ispis
-
-        // TODO:097 Pokrenuti testove i provjeriti da oba testa u grupi TestStatičkogKonstruktora prolaze.
+        // ++TODO:097 Pokrenuti testove i provjeriti da oba testa u grupi TestStatičkogKonstruktora prolaze.
         public void Metoda2()
         {
             Console.WriteLine("SaStatičkimKonstruktorom2.Metoda2");
