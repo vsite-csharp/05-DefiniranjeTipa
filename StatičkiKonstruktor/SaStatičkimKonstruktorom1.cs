cd2eaa26ceca1a364cc86911e1fe46468e92c4f8
Jakov Baleta <Jakov Baleta@DESKTOP-LEG458K>
2021-05-30 00:10:08 +0200

    Resolve static constructor

--- StatičkiKonstruktor/SaStatičkimKonstruktorom1.cs
+++ StatičkiKonstruktor/SaStatičkimKonstruktorom1.cs
@@ -4,11 +4,17 @@ namespace Vsite.CSharp.DefiniranjeTipa
 {
     class SaStatičkimKonstruktorom1
     {
-        // TODO:091 Dodati prazni konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom1.KonstruktorKlase"
-
-        // TODO:092 Dodati statički konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom1.StatičkiKonstruktor"
-
-        // TODO:093 Pokrenuti program i pogledati ispis
+        // Dodati prazni konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom1.KonstruktorKlase"
+        public SaStatičkimKonstruktorom1()
+        {
+            Console.WriteLine("SaStatičkimKonstruktorom1.KonstruktorKlase");
+        }
+        // Dodati statički konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom1.StatičkiKonstruktor"
+        static SaStatičkimKonstruktorom1()
+        {
+            Console.WriteLine("SaStatičkimKonstruktorom1.StatičkiKonstruktor");
+        }
+        //  Pokrenuti program i pogledati ispis
 
         public static void Metoda1()
         {
--- StatičkiKonstruktor/SaStatičkimKonstruktorom2.cs
+++ StatičkiKonstruktor/SaStatičkimKonstruktorom2.cs
@@ -4,13 +4,20 @@ namespace Vsite.CSharp.DefiniranjeTipa
 {
     class SaStatičkimKonstruktorom2 : SaStatičkimKonstruktorom1
     {
-        // TODO:094 Dodati prazni konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom2.KonstruktorKlase"
+        // Dodati prazni konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom2.KonstruktorKlase"
+        public SaStatičkimKonstruktorom2()
+        {
+            Console.WriteLine("SaStatičkimKonstruktorom2.KonstruktorKlase");
+        }
 
-        // TODO:095 Dodati statički konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom2.StatičkiKonstruktor"
+        // Dodati statički konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom2.StatičkiKonstruktor"
+        static SaStatičkimKonstruktorom2()
+        {
+            Console.WriteLine("SaStatičkimKonstruktorom2.StatičkiKonstruktor");
+        }
+        // Ponovno pokrenuti program i pogledati ispis
 
-        // TODO:096 Ponovno pokrenuti program i pogledati ispis
-
-        // TODO:097 Pokrenuti testove i provjeriti da oba testa u grupi TestStatičkogKonstruktora prolaze.
+        // Pokrenuti testove i provjeriti da oba testa u grupi TestStatičkogKonstruktora prolaze.
         public void Metoda2()
         {
             Console.WriteLine("SaStatičkimKonstruktorom2.Metoda2");
