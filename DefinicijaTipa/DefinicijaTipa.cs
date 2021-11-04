b5ec1110071db2dfe47e1feb14a3318129faa933
Damir Stojanović <DStojanovic@dspace.hr>
2021-11-04 21:19:47 +0100

    Resolve definicija tipa

--- DefinicijaTipa/DefinicijaTipa.cs
+++ DefinicijaTipa/DefinicijaTipa.cs
@@ -5,18 +5,28 @@ namespace Vsite.CSharp.DefiniranjeTipa
     class MojaKlasa
     {
         // TODO:010 Dodati privatno polje (field - podatkovni član) "broj" tipa int i dodijeliti mu vrijednost 5
-
+        private int broj = 5;
         // TODO:011 Zadati javnu metodu IspišiČlan tipa void i u njoj dodati naredbu koja će ispisati podatkovni član "broj".
-
+        public void IspišiČlan()
+        {
+            Console.WriteLine(broj);
+        }
     }
 
 
     struct MojaStruktura
     {
+        public MojaStruktura(string tekst)
+        {
+            this.tekst = tekst;
+        }
         // TODO:013 Dodati privatno polje (podatkovni član) "tekst" tipa string i dodijeliti mu vrijednost "MojaStruktura"
-
+        private string tekst; //="MojaStruktura";
         // TODO:014 Zadati javnu metodu IspišiČlan tipa void i u njoj dodati naredbu koja će ispisati podatkovni član "tekst".
-
+        public void IspišiČlan()
+        {
+            Console.WriteLine(tekst);
+        }
     }
 
 
@@ -25,13 +35,15 @@ namespace Vsite.CSharp.DefiniranjeTipa
         public static void IspišiČlanKlase()
         {
             // TODO:012 Inicijalizirati objekt tipa MojaKlasa i pozvati njegovu metodu IspišiČlan
-
+            var mk = new MojaKlasa();
+            mk.IspišiČlan();
         }
 
         public static void IspišiČlanStrukture()
         {
             // TODO:015 Inicijalizirati objekt tipa MojaStruktura i pozvati njegovu metodu IspišiČlan koja treba ispisati "MojaStruktura".
-
+            var ms = new MojaStruktura("MojaStruktura"); // "nesto drugo alo alo"
+            ms.IspišiČlan();
         }
 
         // TODO:016 Pokrenuti testove i provjeriti prolaze li oba testa u grupi TestDefinicijeTipa.
