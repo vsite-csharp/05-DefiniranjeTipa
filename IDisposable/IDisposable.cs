42befa5f2785e3ac84cd798debe486574fa9ad6c
Ivan Sokolović <isokolovic1@gmail.com>
2021-11-11 20:42:48 +0100

    Resolved dispose

--- IDisposable/IDisposable.cs
+++ IDisposable/IDisposable.cs
@@ -3,10 +3,10 @@ using System.IO;
 
 namespace Vsite.CSharp.DefiniranjeTipa
 {
-    // TODO:122 Deklarirati da klasa implementira IDisposable sučelje te dodati javnu metodu Dispose.
-    // TODO:123 Dodati u klasu metodu protected virtual void Dispose(bool disposing) i u nju prebaciti poziv StreamWriter.Close iz destruktora.
-    // TODO:124 Napraviti pozive metode iz destruktora i iz javne metode Dispose.
-    class RadSdatotekom
+    // :122 Deklarirati da klasa implementira IDisposable sučelje te dodati javnu metodu Dispose.
+    // :123 Dodati u klasu metodu protected virtual void Dispose(bool disposing) i u nju prebaciti poziv StreamWriter.Close iz destruktora.
+    // :124 Napraviti pozive metode iz destruktora i iz javne metode Dispose.
+    class RadSdatotekom : IDisposable 
     {
         private StreamWriter sw;
 
@@ -20,19 +20,42 @@ namespace Vsite.CSharp.DefiniranjeTipa
             sw.Write(tekst);
         }
 
-        // TODO:121 Dodati klasi destruktor koji će pozvati metodu StreamWriter.Close (odn. StreamWriter.Dispose). Pokrenuti program i provjeriti ispis.
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                sw.Close();
+                disposed = true;
+            }
+        }
+        // :121 Dodati klasi destruktor koji će pozvati metodu StreamWriter.Close (odn. StreamWriter.Dispose). Pokrenuti program i provjeriti ispis.
+        ~RadSdatotekom()
+        {
+            //sw.Close();
+            Dispose(false);
+        }
     }
 
     class Disposable
     {
         public static void StvaranjeIPisanjeUDatoteku(string imeDatoteke)
         {
-            RadSdatotekom rd = new RadSdatotekom(imeDatoteke);
-            rd.Zapiši("Ovo je moj upis");
+            //RadSdatotekom rd = new RadSdatotekom(imeDatoteke);
+            //rd.Zapiši("Ovo je moj upis");
 
-            // TODO:125 Dodati poziv metode RadSDatotekom.Dispose.
-
-            // TODO:127 Preraditi kod metode tako da se umjesto metode Dispose koristi blok using.
+            // :125 Dodati poziv metode RadSDatotekom.Dispose.
+            //rd.Dispose();
+            // :127 Preraditi kod metode tako da se umjesto metode Dispose koristi blok using.
+            using(RadSdatotekom rd = new RadSdatotekom(imeDatoteke))
+            {
+                rd.Zapiši("Ovo je moj upis");
+            }
 
         }
 
@@ -42,7 +65,7 @@ namespace Vsite.CSharp.DefiniranjeTipa
             File.Delete(imeDatoteke);
         }
 
-        // TODO:120 Pokrenuti program i pogledati ispis. Provjeriti sadržaj datoteke "moj.txt".
+        // :120 Pokrenuti program i pogledati ispis. Provjeriti sadržaj datoteke "moj.txt".
         static void Main(string[] args)
         {
             try
