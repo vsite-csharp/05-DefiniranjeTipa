6404c7cda663f9fbe71bad498719ab25937c5e68
Marina Letica <MLetica@dspace.hr>
2021-11-11 20:42:51 +0100

    Resolved disposable

--- IDisposable/IDisposable.cs
+++ IDisposable/IDisposable.cs
@@ -3,10 +3,10 @@ using System.IO;
 
 namespace Vsite.CSharp.DefiniranjeTipa
 {
-    // TODO:122 Deklarirati da klasa implementira IDisposable sučelje te dodati javnu metodu Dispose.
-    // TODO:123 Dodati u klasu metodu protected virtual void Dispose(bool disposing) i u nju prebaciti poziv StreamWriter.Close iz destruktora.
-    // TODO:124 Napraviti pozive metode iz destruktora i iz javne metode Dispose.
-    class RadSdatotekom
+    // :122 Deklarirati da klasa implementira IDisposable sučelje te dodati javnu metodu Dispose.
+    // :123 Dodati u klasu metodu protected virtual void Dispose(bool disposing) i u nju prebaciti poziv StreamWriter.Close iz destruktora.
+    // :124 Napraviti pozive metode iz destruktora i iz javne metode Dispose.
+    class RadSdatotekom : IDisposable
     {
         private StreamWriter sw;
 
@@ -20,20 +20,45 @@ namespace Vsite.CSharp.DefiniranjeTipa
             sw.Write(tekst);
         }
 
-        // TODO:121 Dodati klasi destruktor koji će pozvati metodu StreamWriter.Close (odn. StreamWriter.Dispose). Pokrenuti program i provjeriti ispis.
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private bool disposed = false;
+        //moze se pozivati iz finalized metode i eksplicitne iz ove gore
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                sw.Close();
+                disposed = true;
+            }
+        }
+
+        // :121 Dodati klasi destruktor koji će pozvati metodu StreamWriter.Close (odn. StreamWriter.Dispose). Pokrenuti program i provjeriti ispis.
+        ~RadSdatotekom()
+        {
+            Dispose(false);
+        }
     }
 
     class Disposable
     {
         public static void StvaranjeIPisanjeUDatoteku(string imeDatoteke)
         {
-            RadSdatotekom rd = new RadSdatotekom(imeDatoteke);
-            rd.Zapiši("Ovo je moj upis");
+            //RadSdatotekom rd = new RadSdatotekom(imeDatoteke);
+            //rd.Zapiši("Ovo je moj upis");
 
-            // TODO:125 Dodati poziv metode RadSDatotekom.Dispose.
+            // :125 Dodati poziv metode RadSDatotekom.Dispose.
+            //rd.Dispose();
 
-            // TODO:127 Preraditi kod metode tako da se umjesto metode Dispose koristi blok using.
+            // :127 Preraditi kod metode tako da se umjesto metode Dispose koristi blok using.
+            // ovo je nesto kao context manager u pythonu
+            using (RadSdatotekom rd = new RadSdatotekom(imeDatoteke))
+            {
 
+            }
         }
 
         // TODO:126 Staviti točku prekida (breakpoint) na poziv metode BrisanjeDatoteke, pokrenuti program i provjeriti sadržaj datoteke prije brisanja.
