5e506f8a142b8d90ad7bbd7ec21656817dc91fc6
Matteo Madirazza <mamadira@vsite.hr>
2021-11-11 19:54:15 +0100

    resolve constructor klase

--- KonstruktorKlase/Osoba.cs
+++ KonstruktorKlase/Osoba.cs
@@ -4,12 +4,17 @@ namespace Vsite.CSharp.DefiniranjeTipa
 {
     class Osoba
     {
-        public readonly string Ime;
-        public readonly string Prezime;
-        public readonly DateTime DatumRođenja;
+      public readonly string Ime;
+      public readonly string Prezime;
+      public readonly string DatumRođenja;
 
+    public Osoba(string ime, string prezime, DateTime DatumRođenja)
+        {
+            //Ime = "Pero";
+            //Prezime = "Horvat;
+            // DatumRođenja = DateTime.Now
+            Ime = ime;
+            Prezime = prezime;
+        }
         // TODO:061 Dodati prazni konstruktor koji inicijalizira članove objekta na neku vrijednost, ponovno pokrenuti program i provjeriti ispis.
-
-        // TODO:062 Promijeniti prazan konstruktor tako da prima 3 argumenta (ime, prezime, datumRođenja) kojima se inicijaliziraju članovi objekta.
     }
-}
