662f0bf26595eaf04a455f0f024d04f3f85c02d6
Jakov Zorić <jazoric@vsite.hr>
2021-11-11 19:43:31 +0100

    Resolve static counter

--- Class-Struct/Class-Struct.cs
+++ Class-Struct/Class-Struct.cs
@@ -33,7 +33,7 @@ namespace Vsite.CSharp.DefiniranjeTipa
             Console.WriteLine();
             // :023 Otkomentirati donju naredbu.
            /ms2.tekst = "MyStruct";
-            // TODO:024 Ponovno pozvati metodu IspišiČlan za obje instance te obrazložiti rezultat.
+            // :024 Ponovno pozvati metodu IspišiČlan za obje instance te obrazložiti rezultat.
             ms1.IspišiČlan();
             ms2.IspišiČlan();
         }
@@ -42,18 +42,18 @@ namespace Vsite.CSharp.DefiniranjeTipa
         {
             MojaKlasa mk1 = new MojaKlasa();
             MojaKlasa mk2 = mk1;
-            // TODO:026 Napisati naredbe koje pozivaju metodu IspišiČlan za obje instance.
+            // :026 Napisati naredbe koje pozivaju metodu IspišiČlan za obje instance.
             mk1.IspišiČlan();
             mk2.IspišiČlan();
             Console.WriteLine();
             // :027 Otkomentirati donju naredbu.
             mk2.broj = 2;
-            // TODO:028 Ponovno pozvati metodu IspišiČlan za obje instance te obrazložiti rezultat.
+            // :028 Ponovno pozvati metodu IspišiČlan za obje instance te obrazložiti rezultat.
             mk1.IspišiČlan();
             mk2.IspišiČlan();
         }
 
-        // TODO:029 Pokrenuti testove i provjeriti prolaze li oba testa u grupi TestClass_Struct.
+        // :029 Pokrenuti testove i provjeriti prolaze li oba testa u grupi TestClass_Struct.
 
         static void Main(string[] args)
         {
