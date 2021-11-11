d9320c575a87599e0835c40af4a7029607cf2a81
Vedran Olujic <vedranolujic23@gmail.com>
2021-11-11 20:24:40 +0100

    destruktor, gc collect

--- Destruktor/Destruktor.cs
+++ Destruktor/Destruktor.cs
@@ -12,10 +12,16 @@ namespace Vsite.CSharp.DefiniranjeTipa
             }
 
             // TODO:101 Dodati for petlju koja će se ponoviti 100000 puta i unutar koje će se kreirati novi objekti tipa KlasaSDestruktorom. Pokrenuti program i provjeriti redni broj prvog uništenog objekta.
+            for (int i =1; i < 10000; i++)
+                {
+            
+                    var kd = new KlasaSDestruktorom();
+                   }
 
-            Console.WriteLine("GOTOVO!!!");
+            {
+            }
             Console.ReadKey();
-
+            Console.WriteLine("GOTOVO!!!");
             Console.WriteLine("Završio sam 'Main'");
         }
     }
