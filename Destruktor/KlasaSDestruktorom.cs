da1afa9473a09ba0314c77a8fef2c22f77dbdbd8
Ida Midžić <idmidzic@vsite.hr>
2021-11-11 20:21:45 +0100

    Destruktor resolve

--- Destruktor/KlasaSDestruktorom.cs
+++ Destruktor/KlasaSDestruktorom.cs
@@ -15,9 +15,13 @@ namespace Vsite.CSharp.DefiniranjeTipa
             Debug.WriteLine(poruka);
         }
 
-        // TODO:100 Dodati destruktor (tj. finalizirajuću metodu) i njemu napisati naredbe koje će u konzolu i u Output prozor ispisati: "Destruktor objekta br. {RedniBroj}."
-
-
+        // :100 Dodati destruktor (tj. finalizirajuću metodu) i njemu napisati naredbe koje će u konzolu i u Output prozor ispisati: "Destruktor objekta br. {RedniBroj}."
+        ~KlasaSDestruktorom()
+        {
+            Console.WriteLine($"Destruktor br: {RedniBroj}");
+            Debug.WriteLine($"Destruktor br: {RedniBroj}");
+        }
+       
         public readonly int RedniBroj; // redni broj objekta
 
         static int Brojač = 0; // brojač ukupno stvorenih objekata
