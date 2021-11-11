2fe28b47cf002df767a385fcfc8148190a4c15ed
Pavle Pavicic <ppavicic@dspace.hr>
2021-11-11 19:43:28 +0100

    Resolve static counter.

--- Internal2/Internal.cs
+++ Internal2/Internal.cs
@@ -18,19 +18,19 @@ namespace Vsite.CSharp.DefiniranjeTipa
 
         public static void IspišiIzvedenuIzJavne()
         {
-            // TODO:030 Definiciju klase IzvedenaIzJavneBazne promijenite tako da je ona izvedena iz klase JavnaBazna.
+            // Definiciju klase IzvedenaIzJavneBazne promijenite tako da je ona izvedena iz klase JavnaBazna.
             IzvedenaIzJavneBazne izvedenaIzJavne = new IzvedenaIzJavneBazne();
-            // TODO:031 Otkomentirajte donju naredbu, prevedite kod i provjerite radi li poziv ispravno.
-            //izvedenaIzJavne.IspišiMe();
+            // Otkomentirajte donju naredbu, prevedite kod i provjerite radi li poziv ispravno.
+            izvedenaIzJavne.IspišiMe();
         }
 
         public static void IspišiIzvedenuIzBazne()
         {
-            // TODO:032 Definiciju klase IzvedenaIzBazne promijenite tako da je ona izvedena iz klase Bazna. Prevedite kod i popravite pogreške koje prevoditelj prijavljuje.
-            // TODO:033 Promijenite pravo pristupa klasi Bazna da se kod može prevesti.
+            // Definiciju klase IzvedenaIzBazne promijenite tako da je ona izvedena iz klase Bazna. Prevedite kod i popravite pogreške koje prevoditelj prijavljuje.
+            // Promijenite pravo pristupa klasi Bazna da se kod može prevesti.
             IzvedenaIzBazne izvedenaIzBazne = new IzvedenaIzBazne();
-            // TODO:034 Otkomentirajte donju naredbu, prevedite kod i provjerite radi li poziv ispravno.
-            //izvedenaIzBazne.IspišiMe();
+            // Otkomentirajte donju naredbu, prevedite kod i provjerite radi li poziv ispravno.
+            izvedenaIzBazne.IspišiMe();
         }
 
         public static void IspišiIzvedenuIzInterneBazne()
