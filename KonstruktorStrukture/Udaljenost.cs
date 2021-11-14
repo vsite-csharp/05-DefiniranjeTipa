ab368913e9605f2f1bda516a1c8a1ff117bc581d
KaTa1112 <katapogacic1@gmail.com>
2021-11-14 17:21:04 +0100

    Struct constructor

--- KonstruktorStrukture/Udaljenost.cs
+++ KonstruktorStrukture/Udaljenost.cs
@@ -14,10 +14,23 @@
         public readonly double Broj;
         public readonly JedinicaDuljine Jedinica;
 
-        // TODO:071 Dodati prazan konstruktor koji će inicijalizirati članove na neku vrijednost.
-
-        // TODO:073 Promijeniti prazan konstruktor tako da prima dva argumenta ("broj" i "jedinica") kojima se inicijaliziraju članovi. 
-
-        // TODO:075 Dodati konstruktor koji prima samo argument "broj", a član "Jedinica" će konstruktor postaviti na JedinicaDuljine.Metar.
+        //071 Dodati prazan konstruktor koji će inicijalizirati članove na neku vrijednost.
+        //public Udaljenost() --> nije moguće unutar strukture koristiti prazni konstruktor
+        //{
+        //    this.Broj = 2.0;
+        //    this.Jedinica = JedinicaDuljine.Centimetar;
+        //}
+        //073 Promijeniti prazan konstruktor tako da prima dva argumenta ("broj" i "jedinica") kojima se inicijaliziraju članovi. 
+        public Udaljenost(double broj, JedinicaDuljine jedinica)
+        {
+            this.Broj = broj;
+            this.Jedinica = jedinica;
+        }
+        //075 Dodati konstruktor koji prima samo argument "broj", a član "Jedinica" će konstruktor postaviti na JedinicaDuljine.Metar.
+        public Udaljenost(double broj)
+        {
+            this.Broj = broj;
+            this.Jedinica = JedinicaDuljine.Metar;
+        }
     }
 }
