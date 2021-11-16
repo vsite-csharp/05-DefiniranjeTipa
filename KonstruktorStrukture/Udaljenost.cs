0d20dd19514a32f7af2984b74b04eaac513e808a
Antonio Brkic <ABrkic@dspace.hr>
2021-11-16 13:04:38 +0100

    Resolve DZ struct constructors

--- KonstruktorStrukture/Udaljenost.cs
+++ KonstruktorStrukture/Udaljenost.cs
@@ -14,10 +14,19 @@
         public readonly double Broj;
         public readonly JedinicaDuljine Jedinica;
 
-        // TODO:071 Dodati prazan konstruktor koji će inicijalizirati članove na neku vrijednost.
+        // 071 Dodati prazan konstruktor koji će inicijalizirati članove na neku vrijednost.
+        public Udaljenost(double broj, JedinicaDuljine jedinica)
+        {
+            this.Broj = broj;
+            this.Jedinica = jedinica;
+        }
+        // 073 Promijeniti prazan konstruktor tako da prima dva argumenta ("broj" i "jedinica") kojima se inicijaliziraju članovi. 
 
-        // TODO:073 Promijeniti prazan konstruktor tako da prima dva argumenta ("broj" i "jedinica") kojima se inicijaliziraju članovi. 
-
-        // TODO:075 Dodati konstruktor koji prima samo argument "broj", a član "Jedinica" će konstruktor postaviti na JedinicaDuljine.Metar.
+        // 075 Dodati konstruktor koji prima samo argument "broj", a član "Jedinica" će konstruktor postaviti na JedinicaDuljine.Metar.
+        public Udaljenost(double broj)
+        {
+            this.Broj = broj;
+            this.Jedinica = JedinicaDuljine.Metar;
+        }
     }
 }
