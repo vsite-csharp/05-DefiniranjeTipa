b84be2c50362f2a7ed4afbff87873ea919dafba8
Ida Midžić <idmidzic@vsite.hr>
2021-11-17 01:08:20 +0100

    Resolve BazneKlase, Internal i IzvedeneKlase iz Internal1 i Internal2 foldera.

--- Internal2/Internal.cs
+++ Internal2/Internal.cs
@@ -26,21 +26,22 @@ namespace Vsite.CSharp.DefiniranjeTipa
 
         public static void IspišiIzvedenuIzBazne()
         {
-            // TODO:032 Definiciju klase IzvedenaIzBazne promijenite tako da je ona izvedena iz klase Bazna. Prevedite kod i popravite pogreške koje prevoditelj prijavljuje.
-            // TODO:033 Promijenite pravo pristupa klasi Bazna da se kod može prevesti.
+            // :032 Definiciju klase IzvedenaIzBazne promijenite tako da je ona izvedena iz klase Bazna. Prevedite kod i popravite pogreške koje prevoditelj prijavljuje.
+            
+            // :033 Promijenite pravo pristupa klasi Bazna da se kod može prevesti.
             IzvedenaIzBazne izvedenaIzBazne = new IzvedenaIzBazne();
-            // TODO:034 Otkomentirajte donju naredbu, prevedite kod i provjerite radi li poziv ispravno.
-            //izvedenaIzBazne.IspišiMe();
+            // :034 Otkomentirajte donju naredbu, prevedite kod i provjerite radi li poziv ispravno.
+            izvedenaIzBazne.IspišiMe();
         }
 
         public static void IspišiIzvedenuIzInterneBazne()
         {
-            // TODO:035 U klasi IzvedenaIzInterneBazne treba, bez promjene prava pristupa, omogućiti poziv metode IspišiMe iz klase InternaBazna ali bez promjene koda klase InternaBazna!
+            // :035 U klasi IzvedenaIzInterneBazne treba, bez promjene prava pristupa, omogućiti poziv metode IspišiMe iz klase InternaBazna ali bez promjene koda klase InternaBazna!
             IzvedenaIzInterneBazne izvedenaIzInterne = new IzvedenaIzInterneBazne();
-            // TODO:036 Otkomentirati donju naredbu i provjeriti izvođenje.
-            //izvedenaIzInterne.IspišiMe();
+            // :036 Otkomentirati donju naredbu i provjeriti izvođenje.
+            izvedenaIzInterne.IspišiMe();
         }
 
-        // TODO:037 Pokrenuti testove i provjeriti prolaze li sva 3 testa u grupi TestInternal
+        // :037 Pokrenuti testove i provjeriti prolaze li sva 3 testa u grupi TestInternal
     }
 }
