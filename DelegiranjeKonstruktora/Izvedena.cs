8fddeb0f268b2cf2ecbf2d74941ec19c2e90af20
Luka Šestak <lusestak@vsite.hr>
2022-01-27 12:44:49 +0100

    .

--- DelegiranjeKonstruktora/Izvedena.cs
+++ DelegiranjeKonstruktora/Izvedena.cs
@@ -1,16 +1,27 @@
 ﻿namespace Vsite.CSharp.DefiniranjeTipa
 {
-    class Izvedena // : Bazna
+
+    class Izvedena : Bazna
     {
         public readonly int B;
 
-        // TODO:080 Proglasiti da je ova klasa izvedena iz klase Bazna te joj definirati konstruktor s dva argumenta ("a" i "b") tipa int, 
+        // :080 Proglasiti da je ova klasa izvedena iz klase Bazna te joj definirati konstruktor s dva argumenta ("a" i "b") tipa int, 
         // kojima će se inicijalizirati članovi A i B. Konstruktor treba pozivati konstruktor bazne klase s jednim argumentom i proslijediti mu argument a.
-        // TODO:081 U tijelo konstruktora dodati naredbu za ispis: Console.WriteLine(string.Format("Izvedena.Izvedena({0}, {1})", a, b));
+        public Izvedena(int a, int b) : base(a)
+        {
+            this.B = b;
+            System.Console.WriteLine(string.Format("Izvedena.Izvedena({0}, {1})", a, b));
+        }
 
-        // TODO:084 Dodati konstruktor s jednim argumentom "a" tipa int. Taj konstruktor treba pozvati ("delegirati") prethodno definirani konstruktor 
+        // :081 U tijelo konstruktora dodati naredbu za ispis: Console.WriteLine(string.Format("Izvedena.Izvedena({0}, {1})", a, b));
+
+        // :084 Dodati konstruktor s jednim argumentom "a" tipa int. Taj konstruktor treba pozvati ("delegirati") prethodno definirani konstruktor 
         // klase Izvedena s dva argumenta s time da je prvi argument "a", a drugi argument je 0. 
-        // TODO:085 U tijelo konstruktora dodati naredbu za ispis: Console.WriteLine(string.Format("Izvedena.Izvedena({0})", a));
+        public Izvedena(int a) : this(a, 0)
+        {
+            System.Console.WriteLine(string.Format("Izvedena.Izvedena({0})", a));
+        }
+        // :085 U tijelo konstruktora dodati naredbu za ispis: Console.WriteLine(string.Format("Izvedena.Izvedena({0})", a));
 
     }
 }
