46ab3fafcfd78bad00aab7715b6fc313d34eadf9
Marko Bohač <mabohac@vsite.hr>
2022-11-17 21:27:01 +0100

    Resolved constructor delegation

--- DelegiranjeKonstruktora/DelegiranjeKonstruktora.cs
+++ DelegiranjeKonstruktora/DelegiranjeKonstruktora.cs
@@ -2,34 +2,38 @@
 
 namespace Vsite.CSharp.DefiniranjeTipa
 {
-    class DelegiranjeKonstruktora
-    {
+	class DelegiranjeKonstruktora
+	{
 
-        public static void StvoriInstancuIzvedeneKlaseKonstruktoromSDvaArgumenta(int a, int b)
-        {
-            // TODO:082 Stvoriti objekt pozivom tog konstruktora:
-            //Izvedena i = new Izvedena(a, b);
+		public static void StvoriInstancuIzvedeneKlaseKonstruktoromSDvaArgumenta(int a, int b)
+		{
+			// 082 Stvoriti objekt pozivom tog konstruktora:
+			Izvedena i = new Izvedena(a, b);
 
-            // TODO:083 Dodati dvije naredbe koje će ispisati vrijednosti članova A i B stvorenog objekta i, pokrenuti kod i provjeriti ispis.
+			// 083 Dodati dvije naredbe koje će ispisati vrijednosti članova A i B stvorenog objekta i, pokrenuti kod i provjeriti ispis.
+			Console.WriteLine(i.A);
+			Console.WriteLine(i.B);
+		}
+
+		public static void StvoriInstancuIzvedeneKlaseKonstruktoromSJednimArgumentom(int a)
+		{
+			// 086 Stvoriti objekt pozivom tog konstruktora:
+			Izvedena i = new Izvedena(a);
+
+            // 087 Dodati dvije naredbe koje će ispisati vrijednosti članova A i B stvorenog objekta i, pokrenuti kod i provjeriti ispis.
+            Console.WriteLine(i.A);
+            Console.WriteLine(i.B);
         }
 
-        public static void StvoriInstancuIzvedeneKlaseKonstruktoromSJednimArgumentom(int a)
-        {
-            // TODO:086 Stvoriti objekt pozivom tog konstruktora:
-            // Izvedena i = new Izvedena(a);
+		// 088 Pokrenuti testove i provjeriti da oba testa u grupi TestDelegiranjaKonstruktora prolaze.
+		static void Main(string[] args)
+		{
+			StvoriInstancuIzvedeneKlaseKonstruktoromSDvaArgumenta(5, 7);
+			Console.WriteLine();
+			StvoriInstancuIzvedeneKlaseKonstruktoromSJednimArgumentom(5);
 
-            // TODO:087 Dodati dvije naredbe koje će ispisati vrijednosti članova A i B stvorenog objekta i, pokrenuti kod i provjeriti ispis.
-        }
-
-        // TODO:088 Pokrenuti testove i provjeriti da oba testa u grupi TestDelegiranjaKonstruktora prolaze.
-        static void Main(string[] args)
-        {
-            StvoriInstancuIzvedeneKlaseKonstruktoromSDvaArgumenta(5, 7);
-            Console.WriteLine();
-            StvoriInstancuIzvedeneKlaseKonstruktoromSJednimArgumentom(5);
-
-            Console.WriteLine("GOTOVO!!!");
-            Console.ReadKey();
-        }
-    }
+			Console.WriteLine("GOTOVO!!!");
+			Console.ReadKey();
+		}
+	}
 }
