032577e89c88e6834f848d5dcb9b9687edf749d4
Marko Bohač <mabohac@vsite.hr>
2022-11-17 19:34:04 +0100

    Resolved using objects

--- KorištenjeObjekata/KorištenjeObjekata.cs
+++ KorištenjeObjekata/KorištenjeObjekata.cs
@@ -4,54 +4,57 @@ using System.Threading;
 
 namespace Vsite.CSharp.DefiniranjeTipa
 {
-    class KorištenjeObjekata
-    {
-        // TODO:000 Ispisati trenutni datum i vrijeme pomoću statičkog svojstva DateTime.Now
-        public static void IspišiTrenutniDatumVrijeme()
-        {
+	class KorištenjeObjekata
+	{
+		// 000 Ispisati trenutni datum i vrijeme pomoću statičkog svojstva DateTime.Now
+		public static void IspišiTrenutniDatumVrijeme()
+		{
+			Console.WriteLine(DateTime.Now);
+		}
 
-        }
+		// 001 Ispisati današnji datum pomoću statičkog svojstva DateTime.Now i metode DateTime.ToShortDateString
+		public static void IspišiDanašnjiDatum()
+		{
+			Console.WriteLine(DateTime.Now.ToShortDateString());
+		}
 
-        // TODO:001 Ispisati današnji datum pomoću statičkog svojstva DateTime.Now i metode DateTime.ToShortDateString
-        public static void IspišiDanašnjiDatum()
-        {
+		// 002 Ispisati trenutno vrijeme pomoću statičkog svojstva DateTime.Now i metode DateTime.ToShortTimeString
+		public static void IspišiTrenutnoVrijeme()
+		{
+			Console.WriteLine(DateTime.Now.ToShortTimeString());
+		}
 
-        }
+		public static int IzračunajBrojDanaIzmeđuDvaDatuma(int godina1, int mjesec1, int dan1, int godina2, int mjesec2, int dan2)
+		{
+			// 003 Stvoriti objekt "datum1" tipa DateTime tako da se njegovom konstruktoru proslijede godina1, mjesec1 i dan1.
+			DateTime datum1 = new DateTime(godina1, mjesec1, dan1);
 
-        // TODO:002 Ispisati trenutno vrijeme pomoću statičkog svojstva DateTime.Now i metode DateTime.ToShortTimeString
-        public static void IspišiTrenutnoVrijeme()
-        {
+			// 004 Stvoriti objekt "datum2" tipa DateTime tako da se njegovom konstruktoru proslijede godina2, mjesec2 i dan2.
+			DateTime datum2 = new DateTime(godina2, mjesec2, dan2);
 
-        }
+			// 005 Izračunati razliku između datum1 i datum2 (jednostavnim oduzimanjem: datum2 - datum1) i tu razliku dodijeliti varijabli "period" tipa TimeSpan (koja služi za pohranu vremenskog perioda).
+			var raspon = datum2 - datum1;
 
-        public static int IzračunajBrojDanaIzmeđuDvaDatuma(int godina1, int mjesec1, int dan1, int godina2, int mjesec2, int dan2)
-        {
-            // TODO:003 Stvoriti objekt "datum1" tipa DateTime tako da se njegovom konstruktoru proslijede godina1, mjesec1 i dan1.
+			// 006 Budući da metoda mora kao rezultat vratiti izračunati period izražen kao broj dana, iz prethodno izračunatog perioda treba dohvatiti i vratiti svojstvo Days (tj. svojstvo TimeSpan.Days).
+			return raspon.Days;
+		}
 
-            // TODO:004 Stvoriti objekt "datum2" tipa DateTime tako da se njegovom konstruktoru proslijede godina2, mjesec2 i dan2.
+		// 007 Pokrenuti testove i provjeriti da svih 5 testova u grupi TestKorištenjaObjekta prolaze
+		static void Main(string[] args)
+		{
+			Thread.CurrentThread.CurrentCulture = new CultureInfo("hr");
 
-            // TODO:005 Izračunati razliku između datum1 i datum2 (jednostavnim oduzimanjem: datum2 - datum1) i tu razliku dodijeliti varijabli "period" tipa TimeSpan (koja služi za pohranu vremenskog perioda).
+			IspišiTrenutniDatumVrijeme();
 
-            // TODO:006 Budući da metoda mora kao rezultat vratiti izračunati period izražen kao broj dana, iz prethodno izračunatog perioda treba dohvatiti i vratiti svojstvo Days (tj. svojstvo TimeSpan.Days).
-            return 0;
-        }
+			IspišiDanašnjiDatum();
 
-        // TODO:007 Pokrenuti testove i provjeriti da svih 5 testova u grupi TestKorištenjaObjekta prolaze
-        static void Main(string[] args)
-        {
-            Thread.CurrentThread.CurrentCulture = new CultureInfo("hr");
+			IspišiTrenutnoVrijeme();
 
-            IspišiTrenutniDatumVrijeme();
+			Console.WriteLine(IzračunajBrojDanaIzmeđuDvaDatuma(2015, 5, 1, 2016, 5, 1));
 
-            IspišiDanašnjiDatum();
+			Console.WriteLine("GOTOVO!!!");
+			Console.ReadKey();
+		}
 
-            IspišiTrenutnoVrijeme();
-
-            Console.WriteLine(IzračunajBrojDanaIzmeđuDvaDatuma(2015, 5, 1, 2016, 5, 1));
-
-            Console.WriteLine("GOTOVO!!!");
-            Console.ReadKey();
-        }
-
-    }
+	}
 }
