ae4b2655d6abbabbdd71c6eb8c23c48e60b502c4
Gabriel Marvin <gabrielmarvin18@gmail.com>
2022-11-17 20:53:25 +0100

    Resolved Class Constructor

--- KonstruktorKlase/Osoba.cs
+++ KonstruktorKlase/Osoba.cs
@@ -8,8 +8,13 @@ namespace Vsite.CSharp.DefiniranjeTipa
         public readonly string Prezime;
         public readonly DateTime DatumRođenja;
 
-        // TODO:061 Dodati prazni konstruktor koji inicijalizira članove objekta na neku vrijednost, ponovno pokrenuti program i provjeriti ispis.
-
+        // Dodati prazni konstruktor koji inicijalizira članove objekta na neku vrijednost, ponovno pokrenuti program i provjeriti ispis.
+        public Osoba(string ime, string prezime, DateTime datum_rođenja)
+        {
+            Ime = ime;
+            Prezime = prezime;
+            DatumRođenja = datum_rođenja;
+        }
         // TODO:062 Promijeniti prazan konstruktor tako da prima 3 argumenta (ime, prezime, datumRođenja) kojima se inicijaliziraju članovi objekta.
     }
 }
