cf7e1acaf21fd15b2817ad0794e8f554936f71eb
Fran Štambuk <fran.stambuk@gmail.com>
2022-11-17 19:50:19 +0100

    Resolved type definition

--- DefinicijaTipa/DefinicijaTipa.cs
+++ DefinicijaTipa/DefinicijaTipa.cs
@@ -5,7 +5,7 @@ namespace Vsite.CSharp.DefiniranjeTipa
     class MojaKlasa
     {
         // Dodati privatno polje (field - podatkovni član) "broj" tipa int i dodijeliti mu vrijednost 5
-        private int broj;
+        private int broj=5;
         // Zadati javnu metodu IspišiČlan tipa void i u njoj dodati naredbu koja će ispisati podatkovni član "broj".
         public void IspišiČlan()
         {
@@ -16,10 +16,17 @@ namespace Vsite.CSharp.DefiniranjeTipa
 
     struct MojaStruktura
     {
-        // TODO:013 Dodati privatno polje (podatkovni član) "tekst" tipa string i dodijeliti mu vrijednost "MojaStruktura"
-
+        // Dodati privatno polje (podatkovni član) "tekst" tipa string i dodijeliti mu vrijednost "MojaStruktura"
+        private string tekst;
         // TODO:014 Zadati javnu metodu IspišiČlan tipa void i u njoj dodati naredbu koja će ispisati podatkovni član "tekst".
-
+        public void IspišiČlan()
+        {
+            Console.WriteLine(tekst);
+        }
+        public MojaStruktura(string tekst)
+        {
+            this.tekst = tekst; 
+        }
     }
 
 
@@ -27,14 +34,16 @@ namespace Vsite.CSharp.DefiniranjeTipa
     {
         public static void IspišiČlanKlase()
         {
-            // TODO:012 Inicijalizirati objekt tipa MojaKlasa i pozvati njegovu metodu IspišiČlan
-
+            // Inicijalizirati objekt tipa MojaKlasa i pozvati njegovu metodu IspišiČlan
+            var mk = new MojaKlasa();
+            mk.IspišiČlan();
         }
 
         public static void IspišiČlanStrukture()
         {
-            // TODO:015 Inicijalizirati objekt tipa MojaStruktura i pozvati njegovu metodu IspišiČlan koja treba ispisati "MojaStruktura".
-
+            // Inicijalizirati objekt tipa MojaStruktura i pozvati njegovu metodu IspišiČlan koja treba ispisati "MojaStruktura".
+            var ms=new MojaStruktura("MojaStruktura");
+            ms.IspišiČlan();
         }
 
         // TODO:016 Pokrenuti testove i provjeriti prolaze li oba testa u grupi TestDefinicijeTipa.
