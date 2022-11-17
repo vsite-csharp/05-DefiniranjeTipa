52b44303dc393757a78427233b8b4e05eed4dbf2
Marko Bohač <mabohac@vsite.hr>
2022-11-17 20:20:36 +0100

    Resolved internal access rights

--- Internal2/Internal.cs
+++ Internal2/Internal.cs
@@ -2,45 +2,45 @@
 
 namespace Vsite.CSharp.DefiniranjeTipa
 {
-    class Internal
-    {
-        static void Main(string[] args)
-        {
-            IspišiIzvedenuIzJavne();
+	class Internal
+	{
+		static void Main(string[] args)
+		{
+			IspišiIzvedenuIzJavne();
 
-            IspišiIzvedenuIzBazne();
+			IspišiIzvedenuIzBazne();
 
-            IspišiIzvedenuIzInterneBazne();
+			IspišiIzvedenuIzInterneBazne();
 
-            Console.WriteLine("GOTOVO!!!");
-            Console.ReadKey();
-        }
+			Console.WriteLine("GOTOVO!!!");
+			Console.ReadKey();
+		}
 
-        public static void IspišiIzvedenuIzJavne()
-        {
-            // TODO:030 Definiciju klase IzvedenaIzJavneBazne promijenite tako da je ona izvedena iz klase JavnaBazna.
-            IzvedenaIzJavneBazne izvedenaIzJavne = new IzvedenaIzJavneBazne();
-            // TODO:031 Otkomentirajte donju naredbu, prevedite kod i provjerite radi li poziv ispravno.
-            //izvedenaIzJavne.IspišiMe();
-        }
+		public static void IspišiIzvedenuIzJavne()
+		{
+			// 030 Definiciju klase IzvedenaIzJavneBazne promijenite tako da je ona izvedena iz klase JavnaBazna.
+			IzvedenaIzJavneBazne izvedenaIzJavne = new IzvedenaIzJavneBazne();
+			// 031 Otkomentirajte donju naredbu, prevedite kod i provjerite radi li poziv ispravno.
+			izvedenaIzJavne.IspišiMe();
+		}
 
-        public static void IspišiIzvedenuIzBazne()
-        {
-            // TODO:032 Definiciju klase IzvedenaIzBazne promijenite tako da je ona izvedena iz klase Bazna. Prevedite kod i popravite pogreške koje prevoditelj prijavljuje.
-            // TODO:033 Promijenite pravo pristupa klasi Bazna da se kod može prevesti.
-            IzvedenaIzBazne izvedenaIzBazne = new IzvedenaIzBazne();
-            // TODO:034 Otkomentirajte donju naredbu, prevedite kod i provjerite radi li poziv ispravno.
-            //izvedenaIzBazne.IspišiMe();
-        }
+		public static void IspišiIzvedenuIzBazne()
+		{
+			// 032 Definiciju klase IzvedenaIzBazne promijenite tako da je ona izvedena iz klase Bazna. Prevedite kod i popravite pogreške koje prevoditelj prijavljuje.
+			// 033 Promijenite pravo pristupa klasi Bazna da se kod može prevesti.
+			IzvedenaIzBazne izvedenaIzBazne = new IzvedenaIzBazne();
+			// 034 Otkomentirajte donju naredbu, prevedite kod i provjerite radi li poziv ispravno.
+			izvedenaIzBazne.IspišiMe();
+		}
 
-        public static void IspišiIzvedenuIzInterneBazne()
-        {
-            // TODO:035 U klasi IzvedenaIzInterneBazne treba, bez promjene prava pristupa, omogućiti poziv metode IspišiMe iz klase InternaBazna ali bez promjene koda klase InternaBazna!
-            IzvedenaIzInterneBazne izvedenaIzInterne = new IzvedenaIzInterneBazne();
-            // TODO:036 Otkomentirati donju naredbu i provjeriti izvođenje.
-            //izvedenaIzInterne.IspišiMe();
-        }
+		public static void IspišiIzvedenuIzInterneBazne()
+		{
+			// 035 U klasi IzvedenaIzInterneBazne treba, bez promjene prava pristupa, omogućiti poziv metode IspišiMe iz klase InternaBazna ali bez promjene koda klase InternaBazna!
+			IzvedenaIzInterneBazne izvedenaIzInterne = new IzvedenaIzInterneBazne();
+			// 036 Otkomentirati donju naredbu i provjeriti izvođenje.
+			izvedenaIzInterne.IspišiMe();
+		}
 
-        // TODO:037 Pokrenuti testove i provjeriti prolaze li sva 3 testa u grupi TestInternal
-    }
+		// 037 Pokrenuti testove i provjeriti prolaze li sva 3 testa u grupi TestInternal
+	}
 }
