39b5950df1d273c5b823d2cc54aba093f4cd8b38
Gabriel Marvin <gabrielmarvin18@gmail.com>
2022-11-17 21:26:36 +0100

    Resolved Delegating To Constructor

--- KonstruktorStrukture/KonstruktorStrukture.cs
+++ KonstruktorStrukture/KonstruktorStrukture.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.Metrics;
 
 namespace Vsite.CSharp.DefiniranjeTipa
 {
@@ -6,28 +7,31 @@ namespace Vsite.CSharp.DefiniranjeTipa
     {
         public static void PozivPodrazumijevanogKonstruktora()
         {
-            // TODO:070 Stvoriti objekt strukture Udaljenost i ispisati vrijednosti članova.
-
-
-            // TODO:072 Stvoriti objekt pozivom praznog konstruktora te ispisati vrijednosti članova.
-
+            // Stvoriti objekt strukture Udaljenost i ispisati vrijednosti članova.
+            var udaljenost = new Udaljenost();
+            Console.WriteLine(udaljenost.Broj);
+            Console.WriteLine(udaljenost.Jedinica);
+            // Stvoriti objekt pozivom praznog konstruktora te ispisati vrijednosti članova.
 
         }
 
         public static void PozivKonstruktoraSDvaArgumenta(double broj, JedinicaDuljine jedinica)
         {
-            // TODO:074 Stvoriti objekt pozivom konstruktora koji prima dva argumenta te ispisati vrijednosti članova.
-
-
+            // Stvoriti objekt pozivom konstruktora koji prima dva argumenta te ispisati vrijednosti članova.
+            var udaljenost = new Udaljenost(broj, jedinica);
+            Console.WriteLine(udaljenost.Broj);
+            Console.WriteLine(udaljenost.Jedinica);
         }
 
         public static void PozivKonstruktoraSJednimArgumentom(double broj)
         {
-            // TODO:076 Stvoriti objekt pozivom konstruktora s jednim argumentom te ispisati vrijednosti članova.
-
+            // Stvoriti objekt pozivom konstruktora s jednim argumentom te ispisati vrijednosti članova.
+            var udaljenost = new Udaljenost(broj);
+            Console.WriteLine(udaljenost.Broj);
+            Console.WriteLine(udaljenost.Jedinica);
         }
 
-        // TODO:077 Pokrenuti testove i provjeriti prolaze li sva 3 testa u grupi TestKonstruktoraStrukture
+        // Pokrenuti testove i provjeriti prolaze li sva 3 testa u grupi TestKonstruktoraStrukture
 
         static void Main(string[] args)
         {
