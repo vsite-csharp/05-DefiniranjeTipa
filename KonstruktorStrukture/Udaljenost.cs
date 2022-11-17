df066c8a1f7333a31942de6e4e3e8cd936e269ef
Giancarlo <giancarlo.bedek@hotmail.com>
2022-11-17 21:06:53 +0100

    Resolve struct constructor

--- KonstruktorStrukture/Udaljenost.cs
+++ KonstruktorStrukture/Udaljenost.cs
@@ -14,10 +14,20 @@
         public readonly double Broj;
         public readonly JedinicaDuljine Jedinica;
 
-        // TODO:071 Dodati prazan konstruktor koji će inicijalizirati članove na neku vrijednost.
+        // Dodati prazan konstruktor koji će inicijalizirati članove na neku vrijednost.
+        //public Udaljenost(double broj, JedinicaDuljine jedinica)
+        //{
+        //    Broj = broj;
+        //    Jedinica = jedinica;
+        //}
 
-        // TODO:073 Promijeniti prazan konstruktor tako da prima dva argumenta ("broj" i "jedinica") kojima se inicijaliziraju članovi. 
+        // Promijeniti prazan konstruktor tako da prima dva argumenta ("broj" i "jedinica") kojima se inicijaliziraju članovi. 
 
-        // TODO:075 Dodati konstruktor koji prima samo argument "broj", a član "Jedinica" će konstruktor postaviti na JedinicaDuljine.Metar.
+        // Dodati konstruktor koji prima samo argument "broj", a član "Jedinica" će konstruktor postaviti na JedinicaDuljine.Metar.
+        public Udaljenost(double broj, JedinicaDuljine jedinica = JedinicaDuljine.Metar)
+        {
+            Broj = broj;
+            Jedinica = jedinica;
+        }
     }
 }
