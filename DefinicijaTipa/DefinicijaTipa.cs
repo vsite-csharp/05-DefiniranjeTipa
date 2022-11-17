c138f3034836afe3a6ced1fbeacbf34334d48d2c
ipandzicd <ivan.pandzic@gmail.com>
2022-11-17 19:53:06 +0100

    Update DefinicijaTipa.cs

--- DefinicijaTipa/DefinicijaTipa.cs
+++ DefinicijaTipa/DefinicijaTipa.cs
@@ -4,9 +4,12 @@ namespace Vsite.CSharp.DefiniranjeTipa
 {
     class MojaKlasa
     {
-        // TODO:010 Dodati privatno polje (field - podatkovni član) "broj" tipa int i dodijeliti mu vrijednost 5
+        // :010 Dodati privatno polje (field - podatkovni član) "broj" tipa int i dodijeliti mu vrijednost 5
+        int broj;
+
 
         // TODO:011 Zadati javnu metodu IspišiČlan tipa void i u njoj dodati naredbu koja će ispisati podatkovni član "broj".
+        
 
     }
 
@@ -14,8 +17,17 @@ namespace Vsite.CSharp.DefiniranjeTipa
     struct MojaStruktura
     {
         // TODO:013 Dodati privatno polje (podatkovni član) "tekst" tipa string i dodijeliti mu vrijednost "MojaStruktura"
-
+        private string tekst = "MojaStruktura";
         // TODO:014 Zadati javnu metodu IspišiČlan tipa void i u njoj dodati naredbu koja će ispisati podatkovni član "tekst".
+        public void IspišiČlan()
+        {
+            Console.WriteLine(tekst);
+        }
+
+        public MojaStruktura(string tekst)
+        {
+            var ms = new MojaStruktura();
+        }
 
     }
 
@@ -24,13 +36,18 @@ namespace Vsite.CSharp.DefiniranjeTipa
     {
         public static void IspišiČlanKlase()
         {
-            // TODO:012 Inicijalizirati objekt tipa MojaKlasa i pozvati njegovu metodu IspišiČlan
+            // :012 Inicijalizirati objekt tipa MojaKlasa i pozvati njegovu metodu IspišiČlan
+            var mk = new MojaKlasa();
+            mk.IspišiČlan();
+
 
         }
 
         public static void IspišiČlanStrukture()
         {
             // TODO:015 Inicijalizirati objekt tipa MojaStruktura i pozvati njegovu metodu IspišiČlan koja treba ispisati "MojaStruktura".
+            var ms = new MojaStruktura();
+            ms.IspišiČlan();
 
         }
 
