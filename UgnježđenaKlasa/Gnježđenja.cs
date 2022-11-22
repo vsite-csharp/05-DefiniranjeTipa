a22d2ae6db9a6c14d6c7b802eb60a8a54a5b7112
Marko Bohač <mabohac@vsite.hr>
2022-11-22 21:39:35 +0100

    Resolved nested classes

--- UgnježđenaKlasa/Gnježđenja.cs
+++ UgnježđenaKlasa/Gnježđenja.cs
@@ -2,40 +2,42 @@
 
 namespace Vsite.CSharp.DefiniranjeTipa
 {
-    class Gnijezdo
-    {
-        class Ugnježđena
-        {
-            public void IspišiMe()
-            {
-                Console.WriteLine("Ugnježđena");
-            }
+	class Gnijezdo
+	{
+		internal class Ugnježđena
+		{
+			public void IspišiMe()
+			{
+				Console.WriteLine("Ugnježđena");
+			}
 
-            protected void PredstaviSe()
-            {
-                Console.WriteLine("Ja sam klasa Ugnježđena");
-            }
+			public void PredstaviSe()
+			{
+				Console.WriteLine("Ja sam klasa Ugnježđena");
+			}
 
-            public void PredstaviSeNeposredno()
-            {
-                Console.WriteLine("Ja sam klasa Ugnježđena, neposredno");
-            }
-        }
+			public void PredstaviSeNeposredno()
+			{
+				Console.WriteLine("Ja sam klasa Ugnježđena, neposredno");
+			}
+		}
 
-        // TODO:040 Napisati implementaciju metode IspišiMe koja će pozvati metodu IspišiMe iz klase Ugnježđena
-        public void IspišiMe()
-        {
-            
-        }
-    }
+		// 040 Napisati implementaciju metode IspišiMe koja će pozvati metodu IspišiMe iz klase Ugnježđena
+		public void IspišiMe()
+		{
+			Ugnježđena tmpUgnježđena = new Ugnježđena();
+			tmpUgnježđena.IspišiMe();
+		}
+	}
 
-    class IzvedenoGnijezdo : Gnijezdo
-    {
-        // TODO:041 Napisati implementaciju metode PredstaviSe koja će pozvati najdostupniju metodu iz klase Ugnježđena
-        // (po potrebi promijeniti pravo pristupa za klasu Ugnježđena)
-        public void PredstaviSe()
-        {
-            
-        }
-    }
+	class IzvedenoGnijezdo : Gnijezdo
+	{
+		// 041 Napisati implementaciju metode PredstaviSe koja će pozvati najdostupniju metodu iz klase Ugnježđena
+		// (po potrebi promijeniti pravo pristupa za klasu Ugnježđena)
+		public void PredstaviSe()
+		{
+			Ugnježđena tmpUgnježđena = new Ugnježđena();
+			tmpUgnježđena.PredstaviSe();
+		}
+	}
 }
