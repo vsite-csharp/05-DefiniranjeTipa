26357ec5e1f358a85720ecb89cafb10965b5d408
Marko Bohač <mabohac@vsite.hr>
2022-11-24 20:03:01 +0100

    Resolved destructors

--- Destruktor/KlasaSDestruktorom.cs
+++ Destruktor/KlasaSDestruktorom.cs
@@ -3,23 +3,27 @@ using System.Diagnostics;
 
 namespace Vsite.CSharp.DefiniranjeTipa
 {
-    public class KlasaSDestruktorom
-    {
-        public KlasaSDestruktorom()
-        {
-            RedniBroj = ++Brojač;
-            string poruka = string.Format("Konstruktor objekta br. {0}", RedniBroj);
-            // ispis u konzolu
-            Console.WriteLine(poruka);
-            // dijagnostički ispis u Output prozor Visual Studija
-            Debug.WriteLine(poruka);
-        }
+	public class KlasaSDestruktorom
+	{
+		public KlasaSDestruktorom()
+		{
+			RedniBroj = ++Brojač;
+			string poruka = string.Format("Konstruktor objekta br. {0}", RedniBroj);
+			// ispis u konzolu
+			Console.WriteLine(poruka);
+			// dijagnostički ispis u Output prozor Visual Studija
+			Debug.WriteLine(poruka);
+		}
 
-        // TODO:100 Dodati destruktor (tj. finalizirajuću metodu) i njemu napisati naredbe koje će u konzolu i u Output prozor ispisati: "Destruktor objekta br. {RedniBroj}"
+		// 100 Dodati destruktor (tj. finalizirajuću metodu) i njemu napisati naredbe koje će u konzolu i u Output prozor ispisati: "Destruktor objekta br. {RedniBroj}"
+		~KlasaSDestruktorom()
+		{
+            Debug.WriteLine($"Destruktor objekta br. {RedniBroj}");
+            Console.WriteLine($"Destruktor objekta br. {RedniBroj}");
+		}
 
+		public readonly int RedniBroj; // redni broj objekta
 
-        public readonly int RedniBroj; // redni broj objekta
-
-        static int Brojač = 0; // brojač ukupno stvorenih objekata
-    }
+		static int Brojač = 0; // brojač ukupno stvorenih objekata
+	}
 }
