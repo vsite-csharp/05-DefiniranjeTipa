12bb79f7c8ee0d4ecf8cdf93bf717ca51534552f
PatrikBacic <patrikbacic@gmail.com>
2022-11-24 19:34:40 +0100

    Resolve static constructor

--- UgnježđenaKlasa/UgnježđenaKlasa.cs
+++ UgnježđenaKlasa/UgnježđenaKlasa.cs
@@ -21,13 +21,13 @@ namespace Vsite.CSharp.DefiniranjeTipa
 
         public static void IzravnoKorištenjeUgnježđeneKlase()
         {
-            // TODO:042 Modificirati definicije tako da se klasa Ugnježđena može pozivati izravno:
+            // :042 Modificirati definicije tako da se klasa Ugnježđena može pozivati izravno:
             
             Gnijezdo.Ugnježđena gu = new Gnijezdo.Ugnježđena();
             gu.PredstaviSeNeposredno();
             
         }
 
-        // TODO:043 Pokrenuti testove i provjeriti prolaze li sva 3 testa u grupi TestUgnježđena.
+        // :043 Pokrenuti testove i provjeriti prolaze li sva 3 testa u grupi TestUgnježđena.
     }
 }
