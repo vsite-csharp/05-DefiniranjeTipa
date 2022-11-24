6362a7b2e2b848a6f5676eac5f9c41692ec6072d
Nataša Štrangar <nastrang@vsite.hr>
2022-11-24 20:02:49 +0100

    Resolve Destructor

--- Destruktor/Destruktor.cs
+++ Destruktor/Destruktor.cs
@@ -6,12 +6,20 @@ namespace Vsite.CSharp.DefiniranjeTipa
     {
         static void Main(string[] args)
         {
+            var mem = GC.GetGCMemoryInfo().TotalAvailableMemoryBytes;
+            Console.WriteLine(mem/1024/1024);
+
             Console.WriteLine("Ušao sam u 'Main'");
             {
                 KlasaSDestruktorom ksd = new KlasaSDestruktorom();
             }
 
-            // TODO:101 Dodati for petlju koja će se ponoviti 100000 puta i unutar koje će se kreirati novi objekti tipa KlasaSDestruktorom. Pokrenuti program i provjeriti redni broj prvog uništenog objekta.
+            // 101 Dodati for petlju koja će se ponoviti 100000 puta i unutar koje će se kreirati novi objekti tipa KlasaSDestruktorom. Pokrenuti program i provjeriti redni broj prvog uništenog objekta.
+            for (int i = 0; i < 10000; ++i)
+            {
+                KlasaSDestruktorom ksd = new KlasaSDestruktorom();
+                var dummy = new byte[mem / 1000];
+            }
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey();
