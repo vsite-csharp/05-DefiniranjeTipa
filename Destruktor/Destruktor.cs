74f89cc163ad17aed0a5067d16accc8b7649385a
Marko Bohač <mabohac@vsite.hr>
2022-11-24 20:07:03 +0100

    Resolved explicit GC.collect

--- Destruktor/Destruktor.cs
+++ Destruktor/Destruktor.cs
@@ -15,7 +15,7 @@ namespace Vsite.CSharp.DefiniranjeTipa
             }
 
             // 101 Dodati for petlju koja će se ponoviti 100000 puta i unutar koje će se kreirati novi objekti tipa KlasaSDestruktorom. Pokrenuti program i provjeriti redni broj prvog uništenog objekta.
-            for (int i = 0; i < 100000; ++i)
+            for (int i = 0; i < 1000; ++i)
             {
                 KlasaSDestruktorom ksd = new();
                 var dummy = new byte[mem / 1000];
