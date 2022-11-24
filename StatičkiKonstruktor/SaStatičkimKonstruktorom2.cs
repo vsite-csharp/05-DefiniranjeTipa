998f82d52daa95057b81ad50e51a0c6913cea4ca
ana.grlic <ana.grlic@UNIVULAP348>
2022-11-24 19:34:40 +0100

    resolve static constructor

--- StatičkiKonstruktor/SaStatičkimKonstruktorom2.cs
+++ StatičkiKonstruktor/SaStatičkimKonstruktorom2.cs
@@ -5,9 +5,15 @@ namespace Vsite.CSharp.DefiniranjeTipa
     class SaStatičkimKonstruktorom2 : SaStatičkimKonstruktorom1
     {
         // TODO:094 Dodati prazni konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom2.KonstruktorKlase"
-
+        public SaStatičkimKonstruktorom2()
+        {
+            Console.WriteLine("SaStatičkimKonstruktorom2.KonstruktorKlase");
+        }
         // TODO:095 Dodati statički konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom2.StatičkiKonstruktor"
-
+        //public SaStatičkimKonstruktorom2()
+        //{
+        //    Console.WriteLine("SaStatičkimKonstruktorom2.StatičkiKonstruktor");
+        //}
         // TODO:096 Ponovno pokrenuti program i pogledati ispis
 
         // TODO:097 Pokrenuti testove i provjeriti da oba testa u grupi TestStatičkogKonstruktora prolaze.
