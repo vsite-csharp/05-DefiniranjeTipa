b74de2046f1d84721250dcb9145e3790da2f9b33
PatrikBacic <patrikbacic@gmail.com>
2022-11-25 18:32:44 +0100

    Resolve static constructor homework

--- StatičkiKonstruktor/SaStatičkimKonstruktorom2.cs
+++ StatičkiKonstruktor/SaStatičkimKonstruktorom2.cs
@@ -10,7 +10,10 @@ namespace Vsite.CSharp.DefiniranjeTipa
             Console.WriteLine("SaStatičkimKonstruktorom2.KonstruktorKlase");
         }
         // TODO:095 Dodati statički konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom2.StatičkiKonstruktor"
-
+        static SaStatičkimKonstruktorom2()
+        {
+            Console.WriteLine("SaStatičkimKonstruktorom2.StatičkiKonstruktor");
+        }
         // TODO:096 Ponovno pokrenuti program i pogledati ispis
 
         // TODO:097 Pokrenuti testove i provjeriti da oba testa u grupi TestStatičkogKonstruktora prolaze.
