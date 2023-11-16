b18c1463439453b5df6f44fca7f87aa73f3f02a1
Sonja Perković <soperkov@vsite.hr>
2023-11-16 21:22:06 +0100

    resolve class-struct differences

--- Class-Struct/Class-Struct.cs
+++ Class-Struct/Class-Struct.cs
@@ -3,29 +3,43 @@
     // TODO:020 Prekopirati kod strukture iz prethodnog primjera i promijeniti pravo pristupa članu "tekst" u public.
     struct MojaStruktura
     {
-
-
+        public string tekst = "MojaStruktura";
+        public MojaStruktura() { }
+        public void IspišiČlan()
+        {
+            Console.WriteLine(tekst);
+        }
     }
 
     // TODO:025 Prekopirati kod klase iz prethodnog primjera i promijeniti pravo pristupa članu "broj" u public.
     class MojaKlasa
     {
-
+        public int broj = 5;
+        public void IspišiČlan()
+        {
+            Console.WriteLine(broj);
+        }
     }
 
     static class ClassStruct
     {
         public static void IspišiStrukture()
         {
-            // TODO:021 Promijeniti poziv konstruktora tako da ispis bude "MojaStruktura".
+            // DONE:021 Promijeniti poziv konstruktora tako da ispis bude "MojaStruktura".
             MojaStruktura ms1 = new MojaStruktura();
             MojaStruktura ms2 = ms1;
-            // TODO:022 Pozvati metodu IspišiČlan za obje instance (ms1 i ms2).
-
+            ms1.tekst = "MojaStruktura";
+            // DONE:022 Pozvati metodu IspišiČlan za obje instance (ms1 i ms2).
+            ms1.IspišiČlan();
+            ms2.IspišiČlan();
             Console.WriteLine();
-            // TODO:023 Otkomentirati donju naredbu.
-            //ms2.tekst = "MyStruct";
-            // TODO:024 Ponovno pozvati metodu IspišiČlan za obje instance te obrazložiti rezultat.
+            // DONE:023 Otkomentirati donju naredbu.
+            ms2.tekst = "MyStruct";
+            // DONE:024 Ponovno pozvati metodu IspišiČlan za obje instance te obrazložiti rezultat.
+            ms1.IspišiČlan();
+            ms2.IspišiČlan();
+            //struktura je vrijednosni tip podatka što znači ako promijenimo vrijednost jednom objektu neće se promijenit 
+            //drugi nego će se kreirati kopija tog objekta na drugoj adresi
 
         }
 
@@ -33,16 +47,18 @@
         {
             MojaKlasa mk1 = new MojaKlasa();
             MojaKlasa mk2 = mk1;
-            // TODO:026 Napisati naredbe koje pozivaju metodu IspišiČlan za obje instance.
-
+            // DONE:026 Napisati naredbe koje pozivaju metodu IspišiČlan za obje instance.
+            mk1.IspišiČlan();
+            mk2.IspišiČlan();
             Console.WriteLine();
-            // TODO:027 Otkomentirati donju naredbu.
-            //mk2.broj = 2;
-            // TODO:028 Ponovno pozvati metodu IspišiČlan za obje instance te obrazložiti rezultat.
-
+            // DONE:027 Otkomentirati donju naredbu.
+            mk2.broj = 2;
+            // DONE:028 Ponovno pozvati metodu IspišiČlan za obje instance te obrazložiti rezultat.
+            mk1.IspišiČlan();
+            mk2.IspišiČlan();
         }
 
-        // TODO:029 Pokrenuti testove i provjeriti prolaze li oba testa u grupi TestClass_Struct.
+        // DONE:029 Pokrenuti testove i provjeriti prolaze li oba testa u grupi TestClass_Struct.
 
         static void Main(string[] args)
         {
