f0718218dd1ed9301f56ac81e7674fcafb4cf0ae
Julijan Šribar <julijan.sribar@gmail.com>
2023-11-21 23:06:46 +0100

    Update constructor delegating

--- Testovi/TestDelegiranjaKonstruktora.cs
+++ Testovi/TestDelegiranjaKonstruktora.cs
@@ -8,8 +8,8 @@
         public void PozivKonstruktoraSDvaArgumenta()
         {
             DelegiranjeKonstruktora.StvoriInstancuIzvedeneKlaseKonstruktoromSDvaArgumenta(5, 7);
-            Assert.AreEqual("Bazna.Bazna(5)", cw?.GetString());
-            Assert.AreEqual("Izvedena.Izvedena(5, 7)", cw?.GetString());
+            Assert.AreEqual("Bazna.Bazna(a: 5)", cw?.GetString());
+            Assert.AreEqual("Izvedena.Izvedena(a: 5, b: 7)", cw?.GetString());
             Assert.AreEqual(5, cw?.GetInt());
             Assert.AreEqual(7, cw?.GetInt());
         }
@@ -18,9 +18,9 @@
         public void PozivKonstruktoraSJednimArgumentom()
         {
             DelegiranjeKonstruktora.StvoriInstancuIzvedeneKlaseKonstruktoromSJednimArgumentom(5);
-            Assert.AreEqual("Bazna.Bazna(5)", cw?.GetString());
-            Assert.AreEqual("Izvedena.Izvedena(5, 0)", cw?.GetString());
-            Assert.AreEqual("Izvedena.Izvedena(5)", cw?.GetString());
+            Assert.AreEqual("Bazna.Bazna(a: 5)", cw?.GetString());
+            Assert.AreEqual("Izvedena.Izvedena(a: 5, b: 0)", cw?.GetString());
+            Assert.AreEqual("Izvedena.Izvedena(a: 5)", cw?.GetString());
             Assert.AreEqual(5, cw?.GetInt());
             Assert.AreEqual(0, cw?.GetInt());
         }
