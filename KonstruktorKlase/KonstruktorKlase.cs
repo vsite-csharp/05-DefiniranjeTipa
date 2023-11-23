b510e7421421ff19b21109c01e04b0759ee46cd5
Ivan Fišić <ivfisic@vsite.hr>
2023-11-23 21:15:18 +0100

    Resolve class constructor

--- KonstruktorKlase/KonstruktorKlase.cs
+++ KonstruktorKlase/KonstruktorKlase.cs
@@ -4,17 +4,22 @@
     {
         public static void StvoriObjektPodrazumijevanimKonstruktorom()
         {
-            // TODO:060 Stvoriti objekt klase Osoba i ispisati vrijednosti članova Ime, Prezime, DatumRođenja
-
+            // 060 Stvoriti objekt klase Osoba i ispisati vrijednosti članova Ime, Prezime, DatumRođenja
+            // var o = new Osoba(); - napravili smo svoj pa ne radi
+            //Console.WriteLine(o.Ime);
+            //Console.WriteLine(o.Prezime);
+            //Console.WriteLine(o.DatumRođenja);
         }
 
         public static void StvoriObjektZadanimKonstruktorom(string ime, string prezime, DateTime datumRođenja)
         {
-            // TODO:063 Stvoriti objekt klase Osoba pozivom tog konstruktora i ispisati vrijednosti članova Ime, Prezime, DatumRođenja
-
+            // 063 Stvoriti objekt klase Osoba pozivom tog konstruktora i ispisati vrijednosti članova Ime, Prezime, DatumRođenja
+            Console.WriteLine(ime);
+            Console.WriteLine(prezime);
+            Console.WriteLine(datumRođenja);
         }
 
-        // TODO:064 Pokrenuti testove i provjeriti prolazi li test u grupi TestKonstruktoraKlase
+        // 064 Pokrenuti testove i provjeriti prolazi li test u grupi TestKonstruktoraKlase
 
         static void Main(string[] args)
         {
