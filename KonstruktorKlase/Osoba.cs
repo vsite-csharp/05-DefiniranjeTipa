5cd06c980a70f947fade9532a7c700014731cc55
Bogjenero <bornagjenero1@gmail.com>
2023-11-23 21:15:19 +0100

    Resolve class constracter

--- KonstruktorKlase/Osoba.cs
+++ KonstruktorKlase/Osoba.cs
@@ -6,8 +6,13 @@
         public readonly string Prezime;
         public readonly DateTime DatumRođenja;
 
-        // TODO:061 Dodati prazni konstruktor koji inicijalizira članove objekta na neku vrijednost, ponovno pokrenuti program i provjeriti ispis.
-
-        // TODO:062 Promijeniti prazan konstruktor tako da prima 3 argumenta (ime, prezime, datumRođenja) kojima se inicijaliziraju članovi objekta.
+        // :061 Dodati prazni konstruktor koji inicijalizira članove objekta na neku vrijednost, ponovno pokrenuti program i provjeriti ispis.
+        public Osoba(string ime,string prezime,DateTime datumRodenja) 
+        {
+            Ime = ime;
+            Prezime = prezime;
+            DatumRođenja =datumRodenja;
+        }
+        // :062 Promijeniti prazan konstruktor tako da prima 3 argumenta (ime, prezime, datumRođenja) kojima se inicijaliziraju članovi objekta.
     }
 }
