91495972ea351790dfc63636e666f40e33f3b95d
Đuro Rukavina <djrukavina@vsite.hr>
2023-11-23 21:15:19 +0100

    Resolve class constructor

--- KonstruktorKlase/Osoba.cs
+++ KonstruktorKlase/Osoba.cs
@@ -7,7 +7,19 @@
         public readonly DateTime DatumRođenja;
 
         // TODO:061 Dodati prazni konstruktor koji inicijalizira članove objekta na neku vrijednost, ponovno pokrenuti program i provjeriti ispis.
+        public Osoba()
+        {
+            Ime = "Pero";
+            Prezime = "Perić";
+            DatumRođenja = new DateTime(1993, 12, 3);
+        }
 
         // TODO:062 Promijeniti prazan konstruktor tako da prima 3 argumenta (ime, prezime, datumRođenja) kojima se inicijaliziraju članovi objekta.
+        public Osoba(string ime, string prezime, DateTime datumRođenja)
+        {
+            this.Ime = ime;
+            this.Prezime = prezime;
+            this.DatumRođenja = datumRođenja;
+        }
     }
 }
