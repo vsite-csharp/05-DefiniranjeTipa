d316fcd5500ea3fd02c365b4fd8f325cacf4baaa
Josip Grabušić <jograbus@vsite.hr>
2023-11-23 21:15:50 +0100

    Resolve class constructor

--- KonstruktorKlase/Osoba.cs
+++ KonstruktorKlase/Osoba.cs
@@ -6,8 +6,19 @@
         public readonly string Prezime;
         public readonly DateTime DatumRođenja;
 
-        // TODO:061 Dodati prazni konstruktor koji inicijalizira članove objekta na neku vrijednost, ponovno pokrenuti program i provjeriti ispis.
-
-        // TODO:062 Promijeniti prazan konstruktor tako da prima 3 argumenta (ime, prezime, datumRođenja) kojima se inicijaliziraju članovi objekta.
+        // :061 Dodati prazni konstruktor koji inicijalizira članove objekta na neku vrijednost, ponovno pokrenuti program i provjeriti ispis.
+        public Osoba()
+        {
+            Ime = "Pero";
+            Prezime = "Horvat";
+            DatumRođenja = new DateTime(1993, 12, 3);
+        }
+        //:062 Promijeniti prazan konstruktor tako da prima 3 argumenta (ime, prezime, datumRođenja) kojima se inicijaliziraju članovi objekta.
+        public Osoba(string ime, string prezime, DateTime datumRodjena)
+        {
+            Ime = ime;
+            Prezime = prezime;
+            DatumRođenja = datumRodjena;
+        }
     }
 }
