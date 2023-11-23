fbe1f64ed03eea40c36caaa2a93b49d0c77f73f1
Ivan Fišić <ivfisic@vsite.hr>
2023-11-23 21:29:16 +0100

    Resolve constructor delegation

--- DelegiranjeKonstruktora/DelegiranjeKonstruktora.cs
+++ DelegiranjeKonstruktora/DelegiranjeKonstruktora.cs
@@ -5,21 +5,26 @@
 
         public static void StvoriInstancuIzvedeneKlaseKonstruktoromSDvaArgumenta(int a, int b)
         {
-            // TODO:082 Stvoriti objekt pozivom tog konstruktora:
-            //Izvedena i = new Izvedena(a, b);
+            // 082 Stvoriti objekt pozivom tog konstruktora:
+            Izvedena i = new Izvedena(a, b);
 
-            // TODO:083 Dodati dvije naredbe koje će ispisati vrijednosti članova A i B stvorenog objekta i, pokrenuti kod i provjeriti ispis.
+            // 083 Dodati dvije naredbe koje će ispisati vrijednosti članova A i B stvorenog objekta i, pokrenuti kod i provjeriti ispis.
+            Console.WriteLine(i.A);
+            Console.WriteLine(i.B);
         }
+
 
         public static void StvoriInstancuIzvedeneKlaseKonstruktoromSJednimArgumentom(int a)
         {
-            // TODO:086 Stvoriti objekt pozivom tog konstruktora:
-            // Izvedena i = new Izvedena(a);
+            // 086 Stvoriti objekt pozivom tog konstruktora:
+            Izvedena i = new Izvedena(a);
 
-            // TODO:087 Dodati dvije naredbe koje će ispisati vrijednosti članova A i B stvorenog objekta i, pokrenuti kod i provjeriti ispis.
+            // 087 Dodati dvije naredbe koje će ispisati vrijednosti članova A i B stvorenog objekta i, pokrenuti kod i provjeriti ispis.
+            Console.WriteLine(i.A);
+            Console.WriteLine(i.B);
         }
 
-        // TODO:088 Pokrenuti testove i provjeriti da oba testa u grupi TestDelegiranjaKonstruktora prolaze.
+        // 088 Pokrenuti testove i provjeriti da oba testa u grupi TestDelegiranjaKonstruktora prolaze.
         static void Main(string[] args)
         {
             StvoriInstancuIzvedeneKlaseKonstruktoromSDvaArgumenta(5, 7);
--- DelegiranjeKonstruktora/Izvedena.cs
+++ DelegiranjeKonstruktora/Izvedena.cs
@@ -1,16 +1,26 @@
 ﻿namespace Vsite.CSharp.DefiniranjeTipa
 {
-    class Izvedena // : Bazna
+    class Izvedena : Bazna
     {
         public readonly int B;
 
-        // TODO:080 Proglasiti da je ova klasa izvedena iz klase Bazna te joj definirati konstruktor s dva argumenta ("a" i "b") tipa int, 
-        // kojima će se inicijalizirati članovi A i B. Konstruktor treba pozivati konstruktor bazne klase s jednim argumentom i proslijediti mu argument a.
-        // TODO:081 U tijelo konstruktora dodati naredbu za ispis: Console.WriteLine(string.Format("Izvedena.Izvedena({0}, {1})", a, b));
+        public Izvedena(int a, int b) : base(a)
+        {
+            B = b;
+            Console.WriteLine($"Izvedena.Izvedena({a}, {b})");
+        } 
+        public Izvedena(int a) : this(a, 0)
+        {
+            Console.WriteLine($"Izvedena.Izvedena({a})");
+        }
 
-        // TODO:084 Dodati konstruktor s jednim argumentom "a" tipa int. Taj konstruktor treba pozvati ("delegirati") prethodno definirani konstruktor 
+        // 080 Proglasiti da je ova klasa izvedena iz klase Bazna te joj definirati konstruktor s dva argumenta ("a" i "b") tipa int, 
+        // kojima će se inicijalizirati članovi A i B. Konstruktor treba pozivati konstruktor bazne klase s jednim argumentom i proslijediti mu argument a.
+        // 081 U tijelo konstruktora dodati naredbu za ispis: Console.WriteLine(string.Format("Izvedena.Izvedena({0}, {1})", a, b));
+
+        // 084 Dodati konstruktor s jednim argumentom "a" tipa int. Taj konstruktor treba pozvati ("delegirati") prethodno definirani konstruktor 
         // klase Izvedena s dva argumenta s time da je prvi argument "a", a drugi argument je 0. 
-        // TODO:085 U tijelo konstruktora dodati naredbu za ispis: Console.WriteLine(string.Format("Izvedena.Izvedena({0})", a));
+        // 085 U tijelo konstruktora dodati naredbu za ispis: Console.WriteLine(string.Format("Izvedena.Izvedena({0})", a));
 
     }
 }
