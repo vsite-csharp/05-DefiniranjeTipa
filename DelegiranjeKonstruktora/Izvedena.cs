e91087962166f8268101699c73a30dc8985f2cfd
Hrvoje Janđel <hrjandje@vsite.hr>
2023-11-23 21:30:18 +0100

    resolve constructor delegation

--- DelegiranjeKonstruktora/Izvedena.cs
+++ DelegiranjeKonstruktora/Izvedena.cs
@@ -19,9 +19,9 @@
         // kojima će se inicijalizirati članovi A i B. Konstruktor treba pozivati konstruktor bazne klase s jednim argumentom i proslijediti mu argument a.
         // :081 U tijelo konstruktora dodati naredbu za ispis: Console.WriteLine(string.Format("Izvedena.Izvedena({0}, {1})", a, b));
 
-        // TODO:084 Dodati konstruktor s jednim argumentom "a" tipa int. Taj konstruktor treba pozvati ("delegirati") prethodno definirani konstruktor 
+        // :084 Dodati konstruktor s jednim argumentom "a" tipa int. Taj konstruktor treba pozvati ("delegirati") prethodno definirani konstruktor 
         // klase Izvedena s dva argumenta s time da je prvi argument "a", a drugi argument je 0. 
-        // TODO:085 U tijelo konstruktora dodati naredbu za ispis: Console.WriteLine(string.Format("Izvedena.Izvedena({0})", a));
+        // :085 U tijelo konstruktora dodati naredbu za ispis: Console.WriteLine(string.Format("Izvedena.Izvedena({0})", a));
 
     }
 }
