b664a78d8780b153980b45502ca546a2b6835fed
Đuro Rukavina <djrukavina@vsite.hr>
2023-11-23 20:52:07 +0100

    Resolve internal modifier

--- Internal2/Internal.cs
+++ Internal2/Internal.cs
@@ -19,7 +19,7 @@
             // TODO:030 Definiciju klase IzvedenaIzJavneBazne promijenite tako da je ona izvedena iz klase JavnaBazna.
             IzvedenaIzJavneBazne izvedenaIzJavne = new IzvedenaIzJavneBazne();
             // TODO:031 Otkomentirajte donju naredbu, prevedite kod i provjerite radi li poziv ispravno.
-            //izvedenaIzJavne.IspišiMe();
+            izvedenaIzJavne.IspišiMe();
         }
 
         public static void IspišiIzvedenuIzBazne()
@@ -28,7 +28,7 @@
             // TODO:033 Promijenite pravo pristupa klasi Bazna da se kod može prevesti.
             IzvedenaIzBazne izvedenaIzBazne = new IzvedenaIzBazne();
             // TODO:034 Otkomentirajte donju naredbu, prevedite kod i provjerite radi li poziv ispravno.
-            //izvedenaIzBazne.IspišiMe();
+            izvedenaIzBazne.IspišiMe();
         }
 
         public static void IspišiIzvedenuIzInterneBazne()
@@ -36,7 +36,7 @@
             // TODO:035 U klasi IzvedenaIzInterneBazne treba, bez promjene prava pristupa, omogućiti poziv metode IspišiMe iz klase InternaBazna ali bez promjene koda klase InternaBazna!
             IzvedenaIzInterneBazne izvedenaIzInterne = new IzvedenaIzInterneBazne();
             // TODO:036 Otkomentirati donju naredbu i provjeriti izvođenje.
-            //izvedenaIzInterne.IspišiMe();
+            izvedenaIzInterne.IspišiMe();
         }
 
         // TODO:037 Pokrenuti testove i provjeriti prolaze li sva 3 testa u grupi TestInternal
