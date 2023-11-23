73972d2acf434ca6a6e795772aacda0a1ea99a49
Đuro Rukavina <djrukavina@vsite.hr>
2023-11-23 22:25:53 +0100

    Resolve struct constructor

--- KonstruktorStrukture/KonstruktorStrukture.cs
+++ KonstruktorStrukture/KonstruktorStrukture.cs
@@ -5,24 +5,30 @@
         public static void PozivPodrazumijevanogKonstruktora()
         {
             // TODO:070 Stvoriti objekt strukture Udaljenost i ispisati vrijednosti članova.
-
+            Udaljenost u = new Udaljenost();
+            Console.WriteLine(u.Broj);
+            Console.WriteLine(u.Jedinica);
 
             // TODO:072 Stvoriti objekt pozivom praznog konstruktora te ispisati vrijednosti članova.
-
-
+            Udaljenost u2 = new Udaljenost();
+            Console.WriteLine(u2.Broj);
+            Console.WriteLine(u2.Jedinica);
         }
 
         public static void PozivKonstruktoraSDvaArgumenta(double broj, JedinicaDuljine jedinica)
         {
             // TODO:074 Stvoriti objekt pozivom konstruktora koji prima dva argumenta te ispisati vrijednosti članova.
-
-
+            Udaljenost u = new Udaljenost(broj, jedinica);
+            Console.WriteLine(u.Broj);
+            Console.WriteLine(u.Jedinica);
         }
 
         public static void PozivKonstruktoraSJednimArgumentom(double broj)
         {
             // TODO:076 Stvoriti objekt pozivom konstruktora s jednim argumentom te ispisati vrijednosti članova.
-
+            Udaljenost u = new Udaljenost(3);
+            Console.WriteLine(u.Broj);
+            Console.WriteLine(u.Jedinica);
         }
 
         // TODO:077 Pokrenuti testove i provjeriti prolaze li sva 3 testa u grupi TestKonstruktoraStrukture
--- KonstruktorStrukture/Udaljenost.cs
+++ KonstruktorStrukture/Udaljenost.cs
@@ -15,9 +15,24 @@
         public readonly JedinicaDuljine Jedinica;
 
         // TODO:071 Dodati prazan konstruktor koji će inicijalizirati članove na neku vrijednost.
+        public Udaljenost()
+        {
+            this.Broj = 0;
+            this.Jedinica = JedinicaDuljine.Milimetar;
+        }
 
         // TODO:073 Promijeniti prazan konstruktor tako da prima dva argumenta ("broj" i "jedinica") kojima se inicijaliziraju članovi. 
+        public Udaljenost(double broj, JedinicaDuljine jedinica)
+        {
+            this.Broj = broj;
+            this.Jedinica = jedinica;
+        }
 
         // TODO:075 Dodati konstruktor koji prima samo argument "broj", a član "Jedinica" će konstruktor postaviti na JedinicaDuljine.Metar.
+        public Udaljenost(double broj)
+        {
+            this.Broj = broj;
+            this.Jedinica = JedinicaDuljine.Metar;
+        }
     }
 }
