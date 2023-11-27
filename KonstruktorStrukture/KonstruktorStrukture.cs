fc1513604a78acf28309c7809c9b2162512fba1d
AnMal <junkie_@msn.com>
2023-11-27 10:18:39 +0100

    Resolve struct constructor

--- KonstruktorStrukture/KonstruktorStrukture.cs
+++ KonstruktorStrukture/KonstruktorStrukture.cs
@@ -4,28 +4,36 @@
     {
         public static void PozivPodrazumijevanogKonstruktora()
         {
-            // TODO:070 Stvoriti objekt strukture Udaljenost i ispisati vrijednosti članova.
+            // :070 Stvoriti objekt strukture Udaljenost i ispisati vrijednosti članova.
+            Udaljenost u = new Udaljenost();
+            Console.WriteLine(u.Broj);
+            Console.WriteLine(u.Jedinica);
 
-
-            // TODO:072 Stvoriti objekt pozivom praznog konstruktora te ispisati vrijednosti članova.
+            // :072 Stvoriti objekt pozivom praznog konstruktora te ispisati vrijednosti članova.
+            Udaljenost up = new Udaljenost();
+            Console.WriteLine(up.Broj);
+            Console.WriteLine(up.Jedinica);
 
 
         }
 
         public static void PozivKonstruktoraSDvaArgumenta(double broj, JedinicaDuljine jedinica)
         {
-            // TODO:074 Stvoriti objekt pozivom konstruktora koji prima dva argumenta te ispisati vrijednosti članova.
-
-
+            // :074 Stvoriti objekt pozivom konstruktora koji prima dva argumenta te ispisati vrijednosti članova.
+            Udaljenost u = new Udaljenost(broj, jedinica);
+            Console.WriteLine(u.Broj);
+            Console.WriteLine(u.Jedinica);
         }
 
         public static void PozivKonstruktoraSJednimArgumentom(double broj)
         {
-            // TODO:076 Stvoriti objekt pozivom konstruktora s jednim argumentom te ispisati vrijednosti članova.
-
+            // :076 Stvoriti objekt pozivom konstruktora s jednim argumentom te ispisati vrijednosti članova.
+            Udaljenost u = new Udaljenost(broj);
+            Console.WriteLine(u.Broj);
+            Console.WriteLine(u.Jedinica);
         }
 
-        // TODO:077 Pokrenuti testove i provjeriti prolaze li sva 3 testa u grupi TestKonstruktoraStrukture
+        // :077 Pokrenuti testove i provjeriti prolaze li sva 3 testa u grupi TestKonstruktoraStrukture
 
         static void Main(string[] args)
         {
