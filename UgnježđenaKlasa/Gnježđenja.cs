4114e5f0b7c8f8d048fc7d1ad35354475de59bf8
Julijan Sribar <julijan.sribar@gmail.com>
2023-11-30 19:33:24 +0100

    Resolve class nesting

--- UgnježđenaKlasa/Gnježđenja.cs
+++ UgnježđenaKlasa/Gnježđenja.cs
@@ -2,14 +2,14 @@
 {
     class Gnijezdo
     {
-        class Ugnježđena
+        protected internal class Ugnježđena
         {
             public void IspišiMe()
             {
                 Console.WriteLine("Ugnježđena");
             }
 
-            protected void PredstaviSe()
+            protected internal void PredstaviSe()
             {
                 Console.WriteLine("Ja sam klasa Ugnježđena");
             }
@@ -20,20 +20,23 @@
             }
         }
 
-        // TODO:040 Napisati implementaciju metode IspišiMe koja će pozvati metodu IspišiMe iz klase Ugnježđena
+        // :040 Napisati implementaciju metode IspišiMe koja će pozvati metodu IspišiMe iz klase Ugnježđena
         public void IspišiMe()
         {
-
+            var ug = new Ugnježđena();
+            ug.IspišiMe();
         }
     }
 
     class IzvedenoGnijezdo : Gnijezdo
     {
-        // TODO:041 Napisati implementaciju metode PredstaviSe koja će pozvati najdostupniju metodu iz klase Ugnježđena
+        // :041 Napisati implementaciju metode PredstaviSe koja će pozvati najdostupniju metodu iz klase Ugnježđena
         // (po potrebi promijeniti pravo pristupa za klasu Ugnježđena)
         public void PredstaviSe()
         {
-            
+            var ug = new Gnijezdo.Ugnježđena();
+            ug.PredstaviSe();
+
         }
     }
 }
