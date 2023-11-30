7d79c9868a8357a909b1c7edf367d8b5f284d7db
Julijan Sribar <julijan.sribar@gmail.com>
2023-11-30 19:33:10 +0100

    Resolve static constructor

--- StatičkiKonstruktor/SaStatičkimKonstruktorom1.cs
+++ StatičkiKonstruktor/SaStatičkimKonstruktorom1.cs
@@ -2,11 +2,19 @@
 {
     class SaStatičkimKonstruktorom1
     {
-        // TODO:091 Dodati prazni konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom1.KonstruktorKlase"
+        // :091 Dodati prazni konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom1.KonstruktorKlase"
+        public SaStatičkimKonstruktorom1()
+        {
+            Console.WriteLine("SaStatičkimKonstruktorom1.KonstruktorKlase");
+        }
 
-        // TODO:092 Dodati statički konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom1.StatičkiKonstruktor"
+        // :092 Dodati statički konstruktor koji će ispisati poruku "SaStatičkimKonstruktorom1.StatičkiKonstruktor"
+        static SaStatičkimKonstruktorom1()
+        {
+            Console.WriteLine("SaStatičkimKonstruktorom1.StatičkiKonstruktor");
+        }
 
-        // TODO:093 Pokrenuti program i pogledati ispis
+        // :093 Pokrenuti program i pogledati ispis
 
         public static void Metoda1()
         {
