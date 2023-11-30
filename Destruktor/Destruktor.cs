5d1de52f5155548616bc80689346014d5157d719
Đuro Rukavina <djrukavina@vsite.hr>
2023-11-30 20:44:13 +0100

    Resolved implement and test Finalize

--- Destruktor/Destruktor.cs
+++ Destruktor/Destruktor.cs
@@ -10,6 +10,11 @@
             }
 
             // TODO:101 Dodati for petlju koja će se ponoviti 100000 puta i unutar koje će se kreirati novi objekti tipa KlasaSDestruktorom. Pokrenuti program i provjeriti redni broj prvog uništenog objekta.
+            for (int i = 0; i < 100000; ++i)
+            {
+                var ksd = new KlasaSDestruktorom();
+            }
+
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey();
