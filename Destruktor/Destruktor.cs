fde5912c8990288162b41fb22e9a3060551d3cec
DrHrast <petar.huljek@gmail.com>
2023-11-30 20:43:51 +0100

    Implement and test Finalizer.

--- Destruktor/Destruktor.cs
+++ Destruktor/Destruktor.cs
@@ -8,8 +8,11 @@
             {
                 var ksd = new KlasaSDestruktorom();
             }
-
-            // TODO:101 Dodati for petlju koja će se ponoviti 100000 puta i unutar koje će se kreirati novi objekti tipa KlasaSDestruktorom. Pokrenuti program i provjeriti redni broj prvog uništenog objekta.
+            // DID_IT:101 Dodati for petlju koja će se ponoviti 100000 puta i unutar koje će se kreirati novi objekti tipa KlasaSDestruktorom. Pokrenuti program i provjeriti redni broj prvog uništenog objekta.
+            for (int i = 0; i < 1000; ++i)
+            {
+                var ksd = new KlasaSDestruktorom();
+            }
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey();
